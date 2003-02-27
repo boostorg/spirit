46c01fd052bfc2bd845f82c3808f4ac7c84e4cfe
Martin Wille <mw8329@yahoo.com.au>
2003-02-27 17:26:37 +0000

    -- added special handling for test25
    [SVN r17677]

--- example/application/cpp/runtests.sh
+++ example/application/cpp/runtests.sh
@@ -13,6 +13,8 @@
 cpp=./bin/cpp/gcc/debug/runtime-link-dynamic/cpp
 test "X$CPP" != "X" && cpp="$CPP"
 
+keep=1
+
 dir="tmp_test"
 
 if test "$keep" = 0; then
@@ -81,4 +83,62 @@ do
     fi
 done
 
+counter=0
+errors=0
+
+expect_re()
+{
+    counter=`expr $counter + 1`
+    read line
+    count=`echo "$line" | egrep -e "^$2\$" | wc -l`
+    if test $count == 1; then
+        echo "$1/$counter: PASSED"
+    else
+        echo "$1/$counter: FAILED"
+        errors=`expr $errors + 1`
+        echo " expected RE: ^$2\$"
+        echo " found      : $line"
+    fi
+    test $count == 1
+}
+
+#
+# Special case for test25
+#
+test25()
+{
+    counter=0
+    errors=0
+
+    # version is %02d%1d%1d
+    # full version %02d%1d%1d%04d
+    version=`$cpp -v`
+    x=`echo $version | sed 's/^/v1=/;s/\./ v2=/;s/\./ v3=/;s/\./ v4=/'`
+    eval "$x"
+
+    test "$v1" -lt "10" && v1="0$v1"
+    test "$v4" -lt "10" && v4="0$v4"
+    test "$v4" -lt "100" && v4="0$v4"
+    test "$v4" -lt "1000" && v4="0$v4"
+
+    spirit_pp="0x$v1$v2$v3"
+    spirit_pp_version="0x$v1$v2$v3$v4"
+    spirit_pp_version_str="$version"
+
+    expect_re $1 '"__STDC__": *1'
+    expect_re $1 '"__cplusplus": *199711L'
+    expect_re $1 '"__DATE__": *"[A-Z][a-z][a-z] [0-3]?[0-9] 20[0-9][0-9]"'
+    expect_re $1 '"__TIME__": *"[0-2]?[0-9]:[0-5][0-9]:[0-5][0-9]"'
+    expect_re $1 '"__LINE__": *21'
+    expect_re $1 '"__FILE__": *.*test25.cpp'
+    expect_re $1 '"__SPIRIT_PP__": *'"$spirit_pp"
+    expect_re $1 '"__SPIRIT_PP_VERSION__": *'"$spirit_pp_version"
+    expect_re $1 '"__SPIRIT_PP_VERSION_STR__": *"'"$spirit_pp_version_str"'"'
+    expect_re $1 50
+    expect_re $1 test.cpp
+    expect_re $1 54
+    expect_re $1 test.cpp
+}
+
+test25 test_files/test25.cpp < $dir/test25.out
 
