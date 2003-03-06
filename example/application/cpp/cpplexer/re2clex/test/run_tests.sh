dd01d7d90fe65581382e122418a21dfd31a6e6a8
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2003-03-06 17:40:40 +0000

    Removed the cpp sample.
    [SVN r17745]

--- example/application/cpp/cpplexer/re2clex/test/run_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-SRCDIR=`dirname $0`
-./test_lexer $SRCDIR/lextest.in | diff - $SRCDIR/lextest.output || exit 1
-./test_lexer $SRCDIR/test_lexer.in | diff - $SRCDIR/test_lexer.output || exit 1
