dbbf127763ed4c615010b37c9fd2b05bf9602b8f
Joel de Guzman <djowel@gmail.com>
2003-04-02 04:30:42 +0000

    new file_iterator from giovanni
    [SVN r18151]

--- example/fundamental/file_parser.cpp
+++ example/fundamental/file_parser.cpp
@@ -21,84 +21,70 @@
 #include <iostream>
 
 ///////////////////////////////////////////////////////////////////////////////
-
 using namespace boost::spirit;
-
 
 ////////////////////////////////////////////////////////////////////////////
 //
 //  Types
 //
 ////////////////////////////////////////////////////////////////////////////
-
-typedef char                  CharT;
-typedef file_iterator <CharT> IteratorT;
-typedef scanner <IteratorT>   ScannerT;
-typedef rule <ScannerT>       RuleT;
-
+typedef char                    char_t;
+typedef file_iterator<char_t>   iterator_t;
+typedef scanner<iterator_t>     scanner_t;
+typedef rule<scanner_t>         rule_t;
 
 ////////////////////////////////////////////////////////////////////////////
 //
 //  Actions
 //
 ////////////////////////////////////////////////////////////////////////////
-
-void Echo (IteratorT const & rBegin, IteratorT const & rEnd)
+void echo(iterator_t first, iterator_t const& last)
 {
-   while (rBegin < rEnd)
-   {
-      std::cout << *rBegin++;
-   }
+    while (first != last)
+        std::cout << *first++;
 }
-
 
 ////////////////////////////////////////////////////////////////////////////
 //
 //  Main program
 //
 ////////////////////////////////////////////////////////////////////////////
-
-int main (int argc, char * argv [])
+int
+main(int argc, char* argv[])
 {
-   if (2 > argc)
-   {
-      std::cout << "Must specify a filename!\n";
+    if (2 > argc)
+    {
+        std::cout << "Must specify a filename!\n";
+        return -1;
+    }
 
-      return -1;
-   }
+    // Create a file iterator for this file
+    iterator_t first(argv[1]);
 
-   // Create a file iterator for this file
-   IteratorT _Start (argv [1]);
+    if (!first)
+    {
+        std::cout << "Unable to open file!\n";
+        return -1;
+    }
 
-   if (!_Start)
-   {
-      std::cout << "Unable to open file!\n";
+    // Create an EOF iterator
+    iterator_t last = first.make_end();
 
-      return -1;
-   }
+    // A simple rule
+    rule_t r = *(anychar_p);
 
-   // Create an EOF iterator
-   IteratorT _End = _Start.make_end ();
+    // Parse
+    parse_info <iterator_t> info = parse(
+        first,
+        last,
+        r[&echo]
+    );
 
-   // A simple rule
-   RuleT _Rule = *(anychar_p);
-
-   // Parse
-   parse_info <IteratorT> _Info = parse (
-      _Start,
-      _End,
-      _Rule [&Echo]
-      );
-
-   // This really shouldn't fail...
-   if (_Info.full)
-   {
-      std::cout << "Parse succeeded!\n";
-   }
-   else
-   {
-      std::cout << "Parse failed!\n";
-   }
+    // This really shouldn't fail...
+    if (info.full)
+        std::cout << "Parse succeeded!\n";
+    else
+        std::cout << "Parse failed!\n";
 
    return 0;
 }
