ab02505540fa94bc85afcdadd52a655626b61eca
Joel de Guzman <djowel@gmail.com>
2003-04-03 13:47:49 +0000

    update from spirit CVS
    [SVN r18164]

--- include/boost/spirit/iterator/file_iterator.hpp
+++ include/boost/spirit/iterator/file_iterator.hpp
@@ -1,6 +1,6 @@
 /*=============================================================================
     Spirit v1.6.0
-    Copyright (c) 2002 Jeff Westfahl
+    Copyright (c) 2003 Giovanni Bajo
     http://spirit.sourceforge.net/
 
     Permission to copy, use, modify, sell and distribute this software is
@@ -9,300 +9,174 @@
     with no claim as to its suitability for any purpose.
 =============================================================================*/
 
-// This code is inspired by FILEiter.h by Stephen Cleary, and is basically a
-// superset of the functionality provided there. FILEiter.h is available from
-// http://groups.yahoo.com/group/boost/files/2000/IOGlue/IOGlue.zip and has
-// the following copyright notice:
-
-// FILEiter: Input and Output iterators for FILE * objects
-
-// (C) Copyright Stephen Cleary 2000.  Permission to copy, use, modify, sell and
-// distribute this software is granted provided this copyright notice appears
-// in all copies.  This software is provided "as is" without express or implied
-// warranty, and with no claim as to its suitability for any purpose.
-
-//
-// Notes on 64-bit file access:
-//
-// The following systems are known to have 64-bit lseek support. Note that on
-// Linux, you must define _FILE_OFFSET_BITS=64 and _LARGEFILE_SOURCE. This
-// should be done on the compiler command line so that it is used globally.
-//
-//    MSVC  (tested on VC6 SP4)
-//    MINGW (tested on recent CYGWIN)
-//    Linux (recent, tested on Mandrake Linux 8.2 X86 and PPC)
-//    Intel C++ (untested, but it uses system headers and libraries)
-//
-// The following systems are known to not have 64-bit lseek support, and do
-// not seem to export any 64-bit file access functions. This may or may not
-// be a problem for you. I know that I don't have a lot of files larger than
-// 2GB laying around on my hard drives, except for testing. But, you never
-// can tell how people might want to use your software.
-//
-//    Borland 5.5.1
-//    CYGWIN
-//
-// Basically, this iterator should work fine on any files < 2GB in size on any
-// system.
-
-#if !defined FILE_ITERATOR_HPP
-#define      FILE_ITERATOR_HPP
-
 ///////////////////////////////////////////////////////////////////////////////
+//
+//  File Iterator structure
+//
+//  The new structure is designed on layers. The top class (used by the user)
+//  is file_iterator, which implements a full random access iterator through
+//  the file, and some specific member functions (constructor that opens
+//  the file, make_end() to generate the end iterator, operator bool to check
+//  if the file was opened correctly).
+//
+//  file_iterator implements the random access iterator interface by the means
+//  of boost::iterator_adaptor, that is inhering an object created with it.
+//  iterator_adaptor gets a low-level file iterator implementation (with just
+//  a few member functions) and a policy (that basically describes to it how
+//  the low-level file iterator interface is). The advantage is that
+//  with boost::iterator_adaptor only 5 functions are needed to implement
+//  a fully conformant random access iterator, instead of dozens of functions
+//  and operators.
+//
+//  There are two low-level file iterators implemented in this module. The 
+//  first (std_file_iterator) uses cstdio stream functions (fopen/fread), which 
+//  support full buffering, and is available everywhere (it's standard C++).
+//  The second (mmap_file_iterator) is currently available only on Windows 
+//  platforms, and uses memory mapped files, which gives a decent speed boost.
+//
+///////////////////////////////////////////////////////////////////////////////
+//
+//  TODO LIST:
+//
+//  - In the Win32 mmap iterator, we could check if keeping a handle to the 
+//    opened file is really required. If it's not, we can just store the file 
+//    length (for make_end()) and save performance. Notice that this should be 
+//    tested under different Windows versions, the behaviour might change.
+//  - Add some error support (by the means of some exceptions) in case of
+//    low-level I/O failure.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef BOOST_SPIRIT_FILE_ITERATOR_HPP
+#define BOOST_SPIRIT_FILE_ITERATOR_HPP
+
+#include <string>
 #include <boost/config.hpp>
-#include <boost/iterator.hpp>
-#include <cstdio>
-#if _MSL_
-#include <sys/stat.h>
-#else
-#include <sys/types.h>
+#include <boost/iterator_adaptors.hpp>
+
+#if (defined(WIN32) || defined(_WIN32) || defined(__WIN32__)) \
+	&& !defined(BOOST_DISABLE_WIN32)
+#define BOOST_SPIRIT_FILEITERATOR_WINDOWS
 #endif
 
 ///////////////////////////////////////////////////////////////////////////////
 namespace boost { namespace spirit {
 
 ///////////////////////////////////////////////////////////////////////////////
-namespace impl {
+namespace fileiter_impl {
 
-// Systems that don't have a 64-bit off_t, but do have a 64-bit lseek
-#if defined BOOST_MSVC              \
- || defined __MINGW32_VERSION       \
- || defined BOOST_INTEL_CXX_VERSION
+    // Forward declarations
+    template <typename CharT = char>
+    struct std_file_iterator;
+    
+#ifdef BOOST_SPIRIT_FILEITERATOR_WINDOWS
+    template <typename CharT = char>
+    class mmap_file_iterator;
+#endif
 
-   // Always use 64-bit file access
-   typedef __int64 off_t;
+    template <typename BaseT>
+    struct file_iterator_policy;
 
+    /////////////////////////////////////////////////////////////////////////
+    //
+    //  file_iter_generator
+    //
+    //  Template meta-function to invoke boost::iterator_adaptor
+    //  NOTE: This cannot be moved into the implementation file because of
+    //  a bug of MSVC 7.0 and previous versions (base classes types are
+    //  looked up at compilation time, not instantion types, and 
+    //  file_iterator would break).
+    //
+    /////////////////////////////////////////////////////////////////////////
+
+    template <typename BaseIteratorT>
+    struct file_iter_generator
+    {
+    public:
+        typedef BaseIteratorT adapted_t;
+        typedef typename adapted_t::value_type value_type;
+        typedef file_iterator_policy<adapted_t> policy_t;
+
+        typedef boost::iterator_adaptor
+        <
+            adapted_t,
+            policy_t,
+            const value_type, const value_type&, const value_type*,
+            std::random_access_iterator_tag,
+            signed long
+        > type;
+    };
+
+///////////////////////////////////////////////////////////////////////////////
+} /* namespace impl */
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  file_iterator
+//
+//  Iterates through an opened file.
+//
+//  The main iterator interface is implemented by the iterator_adaptors
+//  library, which wraps a conforming iterator interface around the
+//  impl::BaseIterator class. This class merely derives the iterator_adaptors
+//  generated class to implement the custom constructors and make_end()
+//  member function.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+template 
+<
+    typename CharT = char, 
+    typename BaseIteratorT = 
+#ifndef BOOST_SPIRIT_FILEITERATOR_WINDOWS
+        fileiter_impl::std_file_iterator<CharT> 
 #else
-
-   // Use the system defined offset size
-   using ::off_t;
-
+        fileiter_impl::mmap_file_iterator<CharT> 
 #endif
-
-///////////////////////////////////////////////////////////////////////////////
-} // namespace impl
-
-///////////////////////////////////////////////////////////////////////////////
-// warning C4284: return type for 'boost::spirit::file_iterator<char>::operator ->'
-// is 'const char *'
-#if defined BOOST_MSVC
-#pragma warning ( push )
-#pragma warning ( disable : 4284 )
-#endif
-
-///////////////////////////////////////////////////////////////////////////////
-template <typename CharT = char>
+>
 class file_iterator
- : public boost::iterator <
-      std::random_access_iterator_tag,
-      CharT,
-      impl::off_t,
-      CharT const *,
-      CharT const &
-      >
+    : public fileiter_impl::file_iter_generator<BaseIteratorT>::type
 {
-   public:
+private:
+    typedef typename 
+        fileiter_impl::file_iter_generator<BaseIteratorT>::type base_t;
+    typedef typename 
+        fileiter_impl::file_iter_generator<BaseIteratorT>::adapted_t adapted_t;
 
-      // Iterator traits
-      typedef impl::off_t   difference_type;
-      typedef CharT         value_type;
-      typedef CharT const * pointer;
-      typedef CharT const & reference;
+public:
+    file_iterator(std::string fileName)
+        : base_t(adapted_t(fileName))
+    {}
 
-      // Standard constructor. Opens the specified file.
-      explicit file_iterator (char const * pFilename);
+    file_iterator(const base_t& iter)
+        : base_t(iter)
+    {}
 
-      // Destructor
-      ~file_iterator ();
+    inline file_iterator& operator=(const base_t& iter);
+    file_iterator make_end(void);
 
-      // Make an end of file iterator for the current file
-      file_iterator make_end ();
+    // operator bool. This borrows a trick from boost::shared_ptr to avoid
+    //   to interfere with arithmetic operations. The dummy member function
+    //   is needed because there are no other normal member function around
+    //   (make_end() confuses GCC because of its return type)
+    //   (NOTE: The bug has been submitted and it's fixed in 3.4)
+    typedef void (file_iterator::*bool_wrap)(void);
 
-      // Returns false if no file is associated with this iterator
-      operator bool () const;
+    operator bool_wrap()
+    { return (this->base() ? &file_iterator::dummy : 0); }
 
-      // Returns file size in bytes
-      difference_type size () const;
-
-      //
-      // Default Constructible
-      //
-
-      // Default constructor
-      file_iterator ();
-
-      //
-      // Assignable
-      //
-
-      // Copy constructor
-      file_iterator (file_iterator const & rCopyMe);
-
-      // Assignment operator
-      file_iterator & operator = (file_iterator const & rAssignMe);
-
-      // Swap
-      void swap (file_iterator & rA, file_iterator & rB);
-
-      //
-      // Trivial Iterator
-      //
-
-      // Dereference
-      //
-      //    Note: Assigning to this will not fail, but is pointless as this
-      //    iterator is only meant to be read from.
-      reference operator * () const;
-
-      // Member access
-      pointer operator -> () const;
-
-      //
-      // Input and Forward Iterator
-      //
-
-      // Preincrement
-      file_iterator const & operator ++ () const;
-
-      // Postincrement
-      file_iterator const operator ++ (int) const;
-
-      //
-      // Bidirectional Iterator
-      //
-
-      // Predecrement
-      file_iterator const & operator -- () const;
-
-      // Postdecrement
-      file_iterator const operator -- (int) const;
-
-      //
-      // Random Access Iterator
-      //
-
-      // Iterator addition (i += n)
-      file_iterator const & operator += (difference_type Distance) const;
-
-      // Iterator addition (i + n)
-      file_iterator const operator + (difference_type Distance) const;
-
-      // Iterator subtraction (i -= n)
-      file_iterator const & operator -= (difference_type Distance) const;
-
-      // Iterator subtraction (i - n)
-      file_iterator const operator - (difference_type Distance) const;
-
-      // Difference (i - j)
-      difference_type operator - (file_iterator const & rIterator) const;
-
-      // Element
-      value_type operator [] (difference_type Index) const;
-
-      //
-      // Equality Comparable
-      //
-
-      bool operator_equal_to (file_iterator const & rOther) const;
-
-      //
-      // LessThan Comparable
-      //
-
-      bool operator_less_than (file_iterator const & rOther) const;
-
-      bool operator_greater_than (file_iterator const & rOther) const;
-
-   private:
-
-      // Character size
-      BOOST_STATIC_CONSTANT (int, kCharSize = sizeof (CharT));
-
-      // Length of the file in bytes
-      difference_type m_Filesize;
-
-      // File descriptor
-      int m_File;
-
-      // Current offset into the file
-      mutable difference_type m_Offset;
-
-      // The next character to be returned
-      mutable CharT m_NextChar;
-
-      // True if this instance is responsible for closing the file
-      bool m_CloseFile;
+private:
+    void dummy(void) {}
 };
 
-// Iterator addition (n + i)
-template <typename CharT>
-file_iterator <CharT> const operator + (
-   typename file_iterator <CharT>::difference_type   Distance,
-   file_iterator <CharT> const                     & rIterator
-   );
-
-//
-// Equality Comparable
-//
-
-// Equality
-template <typename CharT>
-bool operator == (
-   file_iterator <CharT> const & rA,
-   file_iterator <CharT> const & rB
-   );
-
-// Inequality
-template <typename CharT>
-bool operator != (
-   file_iterator <CharT> const & rA,
-   file_iterator <CharT> const & rB
-   );
-
-//
-// LessThan Comparable
-//
-
-// Less
-template <typename CharT>
-bool operator < (
-   file_iterator <CharT> const & rA,
-   file_iterator <CharT> const & rB
-   );
-
-// Greater
-template <typename CharT>
-bool operator > (
-   file_iterator <CharT> const & rA,
-   file_iterator <CharT> const & rB
-   );
-
-// Less or equal
-template <typename CharT>
-bool operator <= (
-   file_iterator <CharT> const & rA,
-   file_iterator <CharT> const & rB
-   );
-
-// Greater or equal
-template <typename CharT>
-bool operator >= (
-   file_iterator <CharT> const & rA,
-   file_iterator <CharT> const & rB
-   );
 
 ///////////////////////////////////////////////////////////////////////////////
-}} // namespace boost::spirit
+}} /* namespace boost::spirit */
+
 
 ///////////////////////////////////////////////////////////////////////////////
-#if defined BOOST_MSVC
-#pragma warning ( pop )
-#endif
+#include "impl/file_iterator.ipp"  /* implementation */
 
-///////////////////////////////////////////////////////////////////////////////
-#endif // #if !defined  FILE_ITERATOR_HPP
+#endif /* BOOST_SPIRIT_FILE_ITERATOR_HPP */
 
-///////////////////////////////////////////////////////////////////////////////
-#if !defined FILE_ITERATOR_IPP
-   #include "boost/spirit/iterator/impl/file_iterator.ipp"
-#endif
