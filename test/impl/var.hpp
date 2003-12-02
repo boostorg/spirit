20a3b0c3defe646562c17109ae84240cded35816
Joel de Guzman <djowel@gmail.com>
2003-12-02 01:58:15 +0000

    Move from Spirit::RELEASE_1_8_0
    [SVN r21057]

--- test/impl/var.hpp
+++ test/impl/var.hpp
@@ -1,13 +1,11 @@
 /*=============================================================================
-  Spirit V1.6.1
-  Copyright (c) 2003 Martin Wille
-  http://spirit.sourceforge.net/
+    Copyright (c) 2003 Martin Wille
+    http://spirit.sourceforge.net/
 
-  Permission to copy, use, modify, sell and distribute this software
-  is granted provided this copyright notice appears in all copies.
-  This software is provided "as is" without express or implied
-  warranty, and with no claim as to its suitability for any purpose.
- =============================================================================*/
+    Use, modification and distribution is subject to the Boost Software
+    License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+    http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
 #ifndef BOOST_SPIRIT_TEST_IMPL_VAR_HPP
 #define BOOST_SPIRIT_TEST_IMPL_VAR_HPP
 
