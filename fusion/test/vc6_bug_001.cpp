d13d09847bbd02326a081d948e7ea105745b2390
Joel de Guzman <djowel@gmail.com>
2004-07-20 00:50:31 +0000

    Fixed Line Endings
    [SVN r23818]

--- fusion/test/vc6_bug_001.cpp
+++ fusion/test/vc6_bug_001.cpp
@@ -1,3 +1,10 @@
+/*=============================================================================
+    Copyright (c) 2003 Joel de Guzman
+
+    Use, modification and distribution is subject to the Boost Software
+    License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+    http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
 #include <boost/test/minimal.hpp>
 #include <boost/spirit/fusion/sequence/tuple.hpp>
 #include <boost/spirit/fusion/sequence/get.hpp>
