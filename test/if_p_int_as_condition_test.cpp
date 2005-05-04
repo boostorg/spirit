ae1d0e6ff04b6197bf5f3fb6dcd960e123af5abc
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2005-05-04 07:33:08 +0000

    Fixed problems diagnosed by the inspection report.
    [SVN r28651]

--- test/if_p_int_as_condition_test.cpp
+++ test/if_p_int_as_condition_test.cpp
@@ -1,3 +1,11 @@
+/*=============================================================================
+    Copyright (c) 2004 Martin Wille
+    http://spirit.sourceforge.net/
+
+    Use, modification and distribution is subject to the Boost Software
+    License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+    http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
 #include <boost/spirit/core.hpp>
 #include <boost/spirit/dynamic.hpp>
 #include <boost/spirit/tree/ast.hpp>
