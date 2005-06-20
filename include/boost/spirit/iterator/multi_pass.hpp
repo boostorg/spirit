93c55fa0dc5f8c7c808d2d800ae9186a243489bf
Markus Schöpflin <markus.schoepflin@comsoft.de>
2005-06-20 10:39:18 +0000

    Fix for compilers not implementing the resolution to DR45.
    [SVN r29702]

--- include/boost/spirit/iterator/multi_pass.hpp
+++ include/boost/spirit/iterator/multi_pass.hpp
@@ -500,6 +500,12 @@ class inner
             bool was_initialized;
         };
 
+       // Needed by compilers not implementing the resolution to DR45. For
+       // reference, see
+       // http://www.open-std.org/JTC1/SC22/WG21/docs/cwg_defects.html#45.
+
+       friend struct Data;
+
     public:
         typedef result_type value_type;
         typedef
