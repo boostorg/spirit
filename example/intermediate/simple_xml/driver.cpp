346a2ce3afda27dc34c2661297ee7f0fe09a225e
Joel de Guzman <djowel@gmail.com>
2005-11-07 04:21:50 +0000

    tabs to spaces
    [SVN r31585]

--- example/intermediate/simple_xml/driver.cpp
+++ example/intermediate/simple_xml/driver.cpp
@@ -4,38 +4,38 @@
 
 namespace std
 {
-	std::ostream & operator << (std::ostream &os,std::pair<std::string,std::string> const &p)
-	{
-		return os << p.first << '=' << p.second;
-	}
-	
-	std::ostream & operator << (std::ostream &os,const tag &t)
-	{
-		return os << t.id;
-	}
-	
+    std::ostream & operator << (std::ostream &os,std::pair<std::string,std::string> const &p)
+    {
+        return os << p.first << '=' << p.second;
+    }
+    
+    std::ostream & operator << (std::ostream &os,const tag &t)
+    {
+        return os << t.id;
+    }
+    
 }
-		
+        
 int main()
 {
-	const char *test = 
-	//	"<A x=\"1\" y=\"2\"> test 1 </A>"
-	//	"<B x=\"3\" y= \"4\" z = \"10\"> test 3 </B>"
-	//	"<C><A></A><V><W></W></V></C>"
-	//	"<D x=\"4\"/>"
-		"<E>xxx<F>yyy</F>zzz</E>"
-		;
-	std::list<tag>	tags;
-	xml_g	g(tags);
-	
-	if(SP::parse(test,g,SP::comment_p("<---","--->")).full)
-	{
-		std::for_each(tags.begin(),tags.end(),walk_data());
-	}
-	else
-	{
-		std::cout << "parse failed\n";
-	}
+    const char *test = 
+    //  "<A x=\"1\" y=\"2\"> test 1 </A>"
+    //  "<B x=\"3\" y= \"4\" z = \"10\"> test 3 </B>"
+    //  "<C><A></A><V><W></W></V></C>"
+    //  "<D x=\"4\"/>"
+        "<E>xxx<F>yyy</F>zzz</E>"
+        ;
+    std::list<tag>  tags;
+    xml_g   g(tags);
+    
+    if(SP::parse(test,g,SP::comment_p("<---","--->")).full)
+    {
+        std::for_each(tags.begin(),tags.end(),walk_data());
+    }
+    else
+    {
+        std::cout << "parse failed\n";
+    }
 }
 
-			
+            
--- example/intermediate/simple_xml/tag.cpp
+++ example/intermediate/simple_xml/tag.cpp
@@ -4,39 +4,39 @@
 
 namespace
 {
-	struct print_pair
-	{
-		template <class P>
-		void operator () (const P &x)
-		{
-			std::cout << '\t' << x.first << ':' << x.second <<'\n';
-		}
-	};
-	
+    struct print_pair
+    {
+        template <class P>
+        void operator () (const P &x)
+        {
+            std::cout << '\t' << x.first << ':' << x.second <<'\n';
+        }
+    };
+    
 }
 
 void walk_data::operator () (const std::string &x)
 {
-	std::cout << "String:" << x <<'\n';
+    std::cout << "String:" << x <<'\n';
 }
 
 void walk_data::operator () (const tag &t)
 {
-	std::cout << "Tag:" << t.id << '\n';
-	std::cout << "Attributes\n";
-	
-	std::for_each
-	(
-		t.attributes.begin(),
-		t.attributes.end(),
-		print_pair()
-	);
-	std::cout << "Children:\n";
-	std::for_each
-	(
-		t.children.begin(),
-		t.children.end(),
-		boost::apply_visitor(*this)
-	);
-	std::cout << "End of tag:" << t.id << '\n';
+    std::cout << "Tag:" << t.id << '\n';
+    std::cout << "Attributes\n";
+    
+    std::for_each
+    (
+        t.attributes.begin(),
+        t.attributes.end(),
+        print_pair()
+    );
+    std::cout << "Children:\n";
+    std::for_each
+    (
+        t.children.begin(),
+        t.children.end(),
+        boost::apply_visitor(*this)
+    );
+    std::cout << "End of tag:" << t.id << '\n';
 }
