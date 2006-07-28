20e32cabdafdb4f38d03cda97bf6e8eea143a3f7
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2006-07-28 14:56:32 +0000

    Added license and copyrights.
    [SVN r34760]

--- example/intermediate/simple_xml/tag.cpp
+++ example/intermediate/simple_xml/tag.cpp
@@ -1,3 +1,7 @@
+//  Copyright (c) 2005 Carl Barron. Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #include "tag.hpp"
 #include <iostream>
 #include <algorithm>
