c08289b8391dfe454634ce3e65d41b62527d2da8
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2006-09-20 12:34:07 +0000

    Bumped Spirit version.
    [SVN r35223]

--- include/boost/spirit/version.hpp
+++ include/boost/spirit/version.hpp
@@ -25,7 +25,7 @@
 //  This is the version of the current Spirit distribution
 //
 ///////////////////////////////////////////////////////////////////////////////
-#define SPIRIT_VERSION 0x1803
+#define SPIRIT_VERSION 0x1804
 #define SPIRIT_PIZZA_VERSION SPIRIT_DOUBLE_CHEESE  // :-)
 
 #endif // defined(SPIRIT_VERSION_HPP)
