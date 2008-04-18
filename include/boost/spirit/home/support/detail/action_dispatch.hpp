c94a2c87cc9035cf5d57c88dfcea461a70cc07de
Joel de Guzman <djowel@gmail.com>
2008-04-18 00:59:39 +0000

    action-dispatch tweaks for phoenix special handling
    [SVN r44526]

--- include/boost/spirit/home/support/detail/action_dispatch.hpp
+++ include/boost/spirit/home/support/detail/action_dispatch.hpp
@@ -10,11 +10,21 @@
 #define BOOST_SPIRIT_ACTION_DISPATCH_APR_18_2008_0720AM
 
 #include <boost/spirit/home/support/detail/values.hpp>
+#include <boost/spirit/home/phoenix/core/actor.hpp>
 
 namespace boost { namespace spirit { namespace detail
 {
     template <typename F, typename Attribute, typename Context>
     bool action_dispatch(F const& f, Attribute& attr, Context& context)
+    {
+        bool pass = true;
+        f(attr, context, pass);
+        return pass;
+    }
+
+    template <typename Eval, typename Attribute, typename Context>
+    bool action_dispatch(phoenix::actor<Eval> const& f
+      , Attribute& attr, Context& context)
     {
         bool pass = true;
         f(pass_value<Attribute>::call(attr), context, pass);
@@ -26,7 +36,9 @@ namespace boost { namespace spirit { namespace detail
     bool action_dispatch(RT(*f)(A0&, A1&, A2&)
       , Attribute& attr, Context& context)
     {
+        bool pass = true;
         f(attr, context, pass);
+        return pass;
     }
 
     template <typename RT, typename A0, typename A1, typename A2
@@ -34,7 +46,45 @@ namespace boost { namespace spirit { namespace detail
     bool action_dispatch(RT(&f)(A0&, A1&, A2&)
       , Attribute& attr, Context& context)
     {
+        bool pass = true;
         f(attr, context, pass);
+        return pass;
+    }
+
+    template <typename RT, typename A0, typename A1
+      , typename Attribute, typename Context>
+    bool action_dispatch(RT(*f)(A0&, A1&)
+      , Attribute& attr, Context& context)
+    {
+        f(attr, context);
+        return true;
+    }
+
+    template <typename RT, typename A0, typename A1
+      , typename Attribute, typename Context>
+    bool action_dispatch(RT(&f)(A0&, A1&)
+      , Attribute& attr, Context& context)
+    {
+        f(attr, context);
+        return true;
+    }
+
+    template <typename RT, typename A0
+      , typename Attribute, typename Context>
+    bool action_dispatch(RT(*f)(A0&)
+      , Attribute& attr, Context& context)
+    {
+        f(attr);
+        return true;
+    }
+
+    template <typename RT, typename A0
+      , typename Attribute, typename Context>
+    bool action_dispatch(RT(&f)(A0&)
+      , Attribute& attr, Context& context)
+    {
+        f(attr);
+        return true;
     }
 }}}
 
