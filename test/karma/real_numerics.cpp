8285af4c3f2a6795e9610d23d6224327284ba574
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2008-04-20 13:00:21 +0000

    Spirit.Karma: re-enabled real_numerics tests related to math::real_concept.
    [SVN r44622]

--- test/karma/real_numerics.cpp
+++ test/karma/real_numerics.cpp
@@ -7,7 +7,7 @@
 
 #include <limits>
 #include <boost/detail/lightweight_test.hpp>
-// #include <boost/math/concepts/real_concept.hpp>
+#include <boost/math/concepts/real_concept.hpp>
 
 #include <boost/spirit/include/karma_char.hpp>
 #include <boost/spirit/include/karma_numeric.hpp>
@@ -67,12 +67,12 @@ struct signed_policy
 ///////////////////////////////////////////////////////////////////////////////
 //  We need to specialize is_real_lit_tag to allow to use a real_concept as a
 //  literal below
-// namespace boost { namespace spirit 
-// {
-//     template <typename Domain>
-//     struct is_real_lit_tag<boost::math::concepts::real_concept, Domain> 
-//       : boost::mpl::true_ {};
-// }}
+namespace boost { namespace spirit 
+{
+    template <typename Domain>
+    struct is_real_lit_tag<boost::math::concepts::real_concept, Domain> 
+      : boost::mpl::true_ {};
+}}
 
 ///////////////////////////////////////////////////////////////////////////////
 int
@@ -389,52 +389,51 @@ main()
         BOOST_TEST(test(" 0.0", upper[signed_], 0.0));
     }
 
-// these tests currently fail because of a problem in the real_concept class
-//     {
-//         using boost::math::concepts::real_concept;
-//         typedef karma::real_spec<real_concept> custom_type;
-//         custom_type const custom = custom_type();
-// 
-//         BOOST_TEST(test("0.0", custom, real_concept(0.0)));
-//         BOOST_TEST(test("1.0", custom, real_concept(1.0)));
-//         BOOST_TEST(test("1.0", custom, real_concept(1.0001)));
-//         BOOST_TEST(test("1.001", custom, real_concept(1.001)));
-//         BOOST_TEST(test("1.01", custom, real_concept(1.010)));
-//         BOOST_TEST(test("1.1", custom, real_concept(1.100)));
-//         
-//         BOOST_TEST(test("1.234e-04", custom, real_concept(0.00012345)));
-//         BOOST_TEST(test("0.001", custom, real_concept(0.0012345)));
-//         BOOST_TEST(test("0.012", custom, real_concept(0.012345)));
-//         BOOST_TEST(test("0.123", custom, real_concept(0.12345)));
-//         BOOST_TEST(test("1.234", custom, real_concept(1.2345)));
-//         BOOST_TEST(test("12.346", custom, real_concept(12.346)));
-//         BOOST_TEST(test("123.46", custom, real_concept(123.46)));
-//         BOOST_TEST(test("1234.5", custom, real_concept(1234.5)));
-//         BOOST_TEST(test("12342.0", custom, real_concept(12342.)));
-//         BOOST_TEST(test("1.234e05", custom, real_concept(123420.)));
-//         
-//         BOOST_TEST(test("-1.0", custom, real_concept(-1.0)));
-//         BOOST_TEST(test("-1.234", custom, real_concept(-1.2345)));
-//         BOOST_TEST(test("-1.235", custom, real_concept(-1.2346)));
-//         BOOST_TEST(test("-1234.2", custom, real_concept(-1234.2)));
-//         
-//         BOOST_TEST(test("1.0", custom(real_concept(1.0))));
-//         BOOST_TEST(test("1.0", custom(real_concept(1.0001))));
-//         BOOST_TEST(test("1.001", custom(real_concept(1.001))));
-//         BOOST_TEST(test("1.01", custom(real_concept(1.010))));
-//         BOOST_TEST(test("1.1", custom(real_concept(1.100))));
-//         
-//         BOOST_TEST(test("1.234e-04", custom(real_concept(0.00012345))));
-//         BOOST_TEST(test("0.001", custom(real_concept(0.0012345))));
-//         BOOST_TEST(test("0.012", custom(real_concept(0.012345))));
-//         BOOST_TEST(test("0.123", custom(real_concept(0.12345))));
-//         BOOST_TEST(test("1.234", custom(real_concept(1.2345))));
-//         BOOST_TEST(test("12.346", custom(real_concept(12.346))));
-//         BOOST_TEST(test("123.46", custom(real_concept(123.46))));
-//         BOOST_TEST(test("1234.5", custom(real_concept(1234.5))));
-//         BOOST_TEST(test("12342.0", custom(real_concept(12342.))));
-//         BOOST_TEST(test("1.234e05", custom(real_concept(123420.))));
-//     }
+    {
+        using boost::math::concepts::real_concept;
+        typedef karma::real_spec<real_concept> custom_type;
+        custom_type const custom = custom_type();
+
+        BOOST_TEST(test("0.0", custom, real_concept(0.0)));
+        BOOST_TEST(test("1.0", custom, real_concept(1.0)));
+        BOOST_TEST(test("1.0", custom, real_concept(1.0001)));
+        BOOST_TEST(test("1.001", custom, real_concept(1.001)));
+        BOOST_TEST(test("1.01", custom, real_concept(1.010)));
+        BOOST_TEST(test("1.1", custom, real_concept(1.100)));
+        
+        BOOST_TEST(test("1.234e-04", custom, real_concept(0.00012345)));
+        BOOST_TEST(test("0.001", custom, real_concept(0.0012345)));
+        BOOST_TEST(test("0.012", custom, real_concept(0.012345)));
+        BOOST_TEST(test("0.123", custom, real_concept(0.12345)));
+        BOOST_TEST(test("1.234", custom, real_concept(1.2345)));
+        BOOST_TEST(test("12.346", custom, real_concept(12.346)));
+        BOOST_TEST(test("123.46", custom, real_concept(123.46)));
+        BOOST_TEST(test("1234.5", custom, real_concept(1234.5)));
+        BOOST_TEST(test("12342.0", custom, real_concept(12342.)));
+        BOOST_TEST(test("1.234e05", custom, real_concept(123420.)));
+        
+        BOOST_TEST(test("-1.0", custom, real_concept(-1.0)));
+        BOOST_TEST(test("-1.234", custom, real_concept(-1.2345)));
+        BOOST_TEST(test("-1.235", custom, real_concept(-1.2346)));
+        BOOST_TEST(test("-1234.2", custom, real_concept(-1234.2)));
+        
+        BOOST_TEST(test("1.0", custom(real_concept(1.0))));
+        BOOST_TEST(test("1.0", custom(real_concept(1.0001))));
+        BOOST_TEST(test("1.001", custom(real_concept(1.001))));
+        BOOST_TEST(test("1.01", custom(real_concept(1.010))));
+        BOOST_TEST(test("1.1", custom(real_concept(1.100))));
+        
+        BOOST_TEST(test("1.234e-04", custom(real_concept(0.00012345))));
+        BOOST_TEST(test("0.001", custom(real_concept(0.0012345))));
+        BOOST_TEST(test("0.012", custom(real_concept(0.012345))));
+        BOOST_TEST(test("0.123", custom(real_concept(0.12345))));
+        BOOST_TEST(test("1.234", custom(real_concept(1.2345))));
+        BOOST_TEST(test("12.346", custom(real_concept(12.346))));
+        BOOST_TEST(test("123.46", custom(real_concept(123.46))));
+        BOOST_TEST(test("1234.5", custom(real_concept(1234.5))));
+        BOOST_TEST(test("12342.0", custom(real_concept(12342.))));
+        BOOST_TEST(test("1.234e05", custom(real_concept(123420.))));
+    }
     
     return boost::report_errors();
 }
