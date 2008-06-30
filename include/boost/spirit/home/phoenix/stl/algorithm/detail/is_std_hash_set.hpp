b544f7755ef43ba1bfda7192787e5390be8500f1
Joel de Guzman <djowel@gmail.com>
2008-06-30 12:12:01 +0000

    added copyright and license
    [SVN r46899]

--- include/boost/spirit/home/phoenix/stl/algorithm/detail/is_std_hash_set.hpp
+++ include/boost/spirit/home/phoenix/stl/algorithm/detail/is_std_hash_set.hpp
@@ -1,3 +1,11 @@
+// Copyright 2005 Daniel Wallin.
+// Copyright 2005 Joel de Guzman.
+//
+// Use, modification and distribution is subject to the Boost Software
+// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+// Modeled after range_ex, Copyright 2004 Eric Niebler
 ///////////////////////////////////////////////////////////////////////////////
 //
 // is_std_hash_set.hpp
