9fefc9c2fe7a94fac3cda1d3235b685d44691b72
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2008-07-19 14:22:43 +0000

    Spirit: Split the pattern test (it timed out on one of the platforms)
    [SVN r47599]

--- test/karma/pattern.cpp
+++ test/karma/pattern.cpp
@@ -36,62 +36,6 @@ int main()
 
     typedef spirit_test::output_iterator<char>::type outiter_type;
 
-    // basic tests
-    {
-        rule<outiter_type> start;
-
-        start = char_[_1 = 'a'] << int_[_1 = 10] << double_[_1 = 12.4];
-        BOOST_TEST(test("a1012.4", start));
-
-        start = (char_ << int_ << double_)[_1 = 'a', _2 = 10, _3 = 12.4];
-        BOOST_TEST(test("a1012.4", start));
-
-        rule<outiter_type> a, b, c;
-        a = char_[_1 = 'a'];
-        b = int_[_1 = 10];
-        c = double_[_1 = 12.4];
-
-        start = a << b << c;
-        BOOST_TEST(test("a1012.4", start));
-    }
-
-    // basic tests involving a direct parameter
-    {
-        typedef variant<char, int, double> var_type;
-        var_type v ('a');
-
-        rule<outiter_type, var_type()> start;
-
-        start = (char_ | int_ | double_)[_1 = _r0];
-        BOOST_TEST(test("a", start, v));
-
-        v = 10;
-        BOOST_TEST(test("10", start, v));
-        v = 12.4;
-        BOOST_TEST(test("12.4", start, v));
-    }
-
-    {
-        rule<outiter_type, void(char, int, double)> start;
-        fusion::vector<char, int, double> vec('a', 10, 12.4);
-
-        start = char_[_1 = _r1] << int_[_1 = _r2] << double_[_1 = _r3];
-        BOOST_TEST(test("a1012.4", start('a', 10, 12.4)));
-
-        start = (char_ << int_ << double_)[_1 = _r1, _2 = _r2, _3 = _r3];
-        BOOST_TEST(test("a1012.4", start('a', 10, 12.4)));
-
-        rule<outiter_type, void(char)> a;
-        rule<outiter_type, void(int)> b;
-        rule<outiter_type, void(double)> c;
-
-        a = char_[_1 = _r1];
-        b = int_[_1 = _r1];
-        c = double_[_1 = _r1];
-        start = a(_r1) << b(_r2) << c(_r3);
-        BOOST_TEST(test("a1012.4", start('a', 10, 12.4)));
-    }
-
     // test rule parameter propagation
     {
         using boost::phoenix::at_c;
