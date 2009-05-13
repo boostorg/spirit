7a7810f35bbfe1e27237bd9d0342ebadd4a7a077
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2009-05-13 02:15:00 +0000

    Spirit: fixed copyrights
    [SVN r52953]

--- test/lex/matlib.h
+++ test/lex/matlib.h
@@ -1,11 +1,8 @@
-/*
- *  matlib.h
- *  a2
- *
- *  Created by Carl Barron on 5/10/09.
- *  Copyright 2009 __MyCompanyName__. All rights reserved.
- *
- */
+//  Copyright (c) 2001-2009 Hartmut Kaiser
+//  Copyright (c) 2009 Carl Barron
+// 
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying 
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef MATLIB_H_05102009
 #define MATLIB_H_05102009
