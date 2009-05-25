c3697fc5dcda17d2271e199122655895e04d9b4c
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2009-05-25 03:24:42 +0000

    Spirit: added more lexer tests
    [SVN r53236]

--- test/lex/regression003.cpp
+++ test/lex/regression003.cpp
@@ -5,9 +5,14 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <boost/detail/lightweight_test.hpp>
-#include <boost/spirit/include/lex_lexertl.hpp>
+#include <boost/spirit/include/lex_static_lexertl.hpp>
+
 #include <iostream>
-#include <algorithm>
+#include <string>
+#include <vector>
+#include <exception>
+
+#include "matlib_static.h"
 #include "matlib.h"
 
 void test_matrix(std::vector<std::vector<double> > const& x)
@@ -18,18 +23,19 @@ void test_matrix(std::vector<std::vector<double> > const& x)
     BOOST_TEST(x[2].size() == 3 && x[2][0] == 4 && x[2][1] == 5 && x[2][2] == 6);
 }
 
-int main ()
+int main()
 {
     std::string input("[[1,2][3][4,5,6]]");
-
     std::vector<std::vector<double> > results;
+
     typedef std::string::iterator iter;
-    typedef boost::spirit::lex::lexertl::actor_lexer<
-        boost::spirit::lex::lexertl::token<iter> 
+    typedef boost::spirit::lex::lexertl::static_actor_lexer<
+        boost::spirit::lex::lexertl::token<iter>,
+        boost::spirit::lex::lexertl::static_::lexer_matlib
     > lexer_type;
 
     typedef matlib_tokens<lexer_type> matlib_type;
-    matlib_type  matrix(results);
+    matlib_type matrix(results);
     iter first = input.begin();
 
     try {
@@ -37,7 +43,7 @@ int main ()
         test_matrix(results);
     }
     catch (std::runtime_error const& e) {
-        std::cerr << "caught exception: " << e.what() << std::endl;
+        std::cerr << e.what() << '\n';
         BOOST_TEST(false);
     }
     return boost::report_errors();
