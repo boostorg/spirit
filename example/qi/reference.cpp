00a62f1c8391538b60e30a6b50d704b2708fe9f2
Joel de Guzman <djowel@gmail.com>
2009-07-11 05:46:30 +0000

    Doc updates
    [SVN r54879]

--- example/qi/reference.cpp
+++ example/qi/reference.cpp
@@ -5,13 +5,15 @@
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 =============================================================================*/
-//[reference_test
+//[reference_includes
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
 #include <iostream>
 #include <string>
+//]
 
+//[reference_test
 template <typename P>
 void test_parser(char const* input, P const& p)
 {
@@ -24,17 +26,30 @@ void test_parser(char const* input, P const& p)
 }
 //]
 
+//[reference_test_attr
+template <typename P, typename T>
+void test_parser_attr(char const* input, P const& p, T& attr)
+{
+    std::string s(input);
+    std::string::iterator f(s.begin());
+    if (boost::spirit::qi::parse(f, s.end(), p, attr))
+        std::cout << "ok" << std::endl;
+    else
+        std::cout << "fail" << std::endl;
+}
+//]
+
 int
 main()
-{
-    //[reference_using_declarations
-    using boost::spirit::qi::parse;
-    using boost::spirit::qi::lit;
-    using boost::spirit::ascii::string;
-    //]
-    
+{   
     {
-        //[reference_string1_literals
+        //[reference_using_declarations_lit_string
+        using boost::spirit::qi::parse;
+        using boost::spirit::qi::lit;
+        using boost::spirit::ascii::string;
+        //]
+        
+        //[reference_string_literals
         test_parser("boost", "boost");          // plain literal
         test_parser("boost", lit("boost"));     // explicit literal
         test_parser("boost", string("boost"));  // ascii::string
@@ -42,7 +57,11 @@ main()
     }
     
     {
-        //[reference_string1_std_string
+        using boost::spirit::qi::parse;
+        using boost::spirit::qi::lit;
+        using boost::spirit::ascii::string;
+        
+        //[reference_string_std_string
         std::string s("boost");
         test_parser("boost", s);            // direct
         test_parser("boost", lit(s));       // explicit
@@ -51,11 +70,36 @@ main()
     }
     
     {
-        //[reference_string1_phoenix
+        using boost::spirit::qi::parse;
+        using boost::spirit::qi::lit;
+        using boost::spirit::ascii::string;
+        
+        //[reference_string_phoenix
         namespace phx = boost::phoenix;        
         test_parser("boost", phx::val("boost"));            // direct
         test_parser("boost", lit(phx::val("boost")));       // explicit
         test_parser("boost", string(phx::val("boost")));    // ascii::string
+        //]
+    }
+    
+    {
+        //[reference_using_declarations_symbols
+        using boost::spirit::qi::parse;
+        using boost::spirit::qi::symbols;
+        //]
+
+        //[reference_symbols_with_data
+        symbols<char, int> sym;
+
+        sym.add
+            ("Apple", 1)
+            ("Banana", 2)
+            ("orange", 3)
+        ;
+
+        int i;
+        test_parser_attr("Banana", sym, i);
+        std::cout << i << std::endl;
         //]
     }
 
