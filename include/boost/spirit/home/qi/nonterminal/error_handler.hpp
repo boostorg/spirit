9201aa943b4cc1f1b736bd696b3844c8116cbbf8
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2009-07-21 02:29:28 +0000

    Spirit: fixed bogus assertion.
    [SVN r55052]

--- include/boost/spirit/home/qi/nonterminal/error_handler.hpp
+++ include/boost/spirit/home/qi/nonterminal/error_handler.hpp
@@ -124,13 +124,13 @@ namespace boost { namespace spirit { namespace qi
                     {
                         case fail: 
                             BOOST_ASSERT(
-                                traits::is_multi_pass<Iterator>::value &&
-                                action != retry && action != fail);
+                                !traits::is_multi_pass<Iterator>::value ||
+                                (action != retry && action != fail));
                             return false;
                         case retry: 
                             BOOST_ASSERT(
-                                traits::is_multi_pass<Iterator>::value &&
-                                action != retry && action != fail);
+                                !traits::is_multi_pass<Iterator>::value ||
+                                (action != retry && action != fail));
                             continue;
                         case accept: return true;
                         case rethrow: throw x;
