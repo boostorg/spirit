141df64db035d97080ff4b825a271f0422aba721
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2009-07-28 00:38:07 +0000

    Spirit: fixing compilation issues
    [SVN r55218]

--- include/boost/spirit/home/qi/detail/unused_skipper.hpp
+++ include/boost/spirit/home/qi/detail/unused_skipper.hpp
@@ -26,20 +26,19 @@ namespace boost { namespace spirit { namespace qi { namespace detail
     // skipper is of the type unused_skipper. In this case we 
     // re-activate the skipper which was active before the skip[]
     // directive.
-    template <typename Skipper, typename Default>
+    template <typename Skipper>
     inline Skipper const& 
-    get_skipper(unused_skipper<Skipper> const& u, Default const&)
+    get_skipper(unused_skipper<Skipper> const& u)
     {
         return u.skipper;
     }
 
-    // If no surrounding lexeme[] directive was specified we activate
-    // a default skipper to use.
-    template <typename Skipper, typename Default>
-    inline Default const& 
-    get_skipper(Skipper const&, Default const& d)
+    // If no surrounding lexeme[] directive was specified we keep what we got.
+    template <typename Skipper>
+    inline Skipper const& 
+    get_skipper(Skipper const& u)
     {
-        return d;
+        return u;
     }
 
 }}}}
