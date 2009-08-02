6776e40b26cd343973c5719708beb4747931e8d9
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2009-08-02 22:51:46 +0000

    Spirit: finished documenting the karma operator module
    [SVN r55375]

--- example/karma/reference.cpp
+++ example/karma/reference.cpp
@@ -58,13 +58,138 @@ main()
         //[reference_karma_using_declarations_alternative
         using boost::spirit::karma::generate;
         using boost::spirit::karma::double_;
-        using boost::spirit::karma::string;
+        using boost::spirit::karma::ascii::string;
         //]
 
-        //[reference_karma_alternative
-        boost::variant<std::string, double> v(1.0);
-        test_generator_attr("1.0", string | double_, v);
+        //[reference_karma_alternative1
+        boost::variant<std::string, double> v1(1.0);
+        test_generator_attr("1.0", string | double_, v1);
+        test_generator_attr("2.0", string | double_, 2.0);
+        //]
+
+        //[reference_karma_alternative2
+        boost::variant<std::string, double> v2("example");
+        test_generator_attr("example", string | double_, v2);
+        test_generator_attr("example", string | double_, "example");
         //]
     }
+
+    {
+        //[reference_karma_using_declarations_kleene
+        using boost::spirit::karma::generate;
+        using boost::spirit::karma::double_;
+        using boost::spirit::karma::space;
+        //]
+
+        //[reference_karma_kleene
+        std::vector<double> v;
+        v.push_back(1.0);
+        v.push_back(2.0);
+        v.push_back(3.0);
+        test_generator_attr("1.0 2.0 3.0 ", *double_, space, v);
+        //]
+    }
+
+    {
+        //[reference_karma_using_declarations_plus
+        using boost::spirit::karma::generate;
+        using boost::spirit::karma::double_;
+        using boost::spirit::karma::space;
+        //]
+
+        //[reference_karma_plus1
+        std::vector<double> v1;
+        v1.push_back(1.0);
+        v1.push_back(2.0);
+        v1.push_back(3.0);
+        test_generator_attr("1.0 2.0 3.0 ", +double_, space, v1);
+        //]
+
+        //[reference_karma_plus2
+        std::vector<double> v2;               // empty container
+        test_generator_attr("empty", +double_ | "empty", v2);
+        //]
+    }
+
+    {
+        //[reference_karma_using_declarations_list
+        using boost::spirit::karma::generate;
+        using boost::spirit::karma::double_;
+        //]
+
+        //[reference_karma_list
+        std::vector<double> v1;
+        v1.push_back(1.0);
+        test_generator_attr("1.0", double_ % ',', v1);
+
+        v1.push_back(2.0);
+        test_generator_attr("1.0,2.0", double_ % ',', v1);
+        //]
+    }
+
+    {
+        //[reference_karma_using_declarations_optional
+        using boost::spirit::karma::generate;
+        using boost::spirit::karma::double_;
+        //]
+
+        //[reference_karma_optional1
+        boost::optional<double> val(1.0);
+        test_generator_attr("1.0", -double_, val);
+        test_generator_attr("2.0", -double_, 2.0);
+        //]
+
+        //[reference_karma_optional2
+        boost::optional<double> val;          // empty optional
+        test_generator_attr("", -double_, val);
+        //]
+    }
+
+    {
+        //[reference_karma_using_declarations_and_predicate
+        using boost::spirit::karma::generate;
+        using boost::spirit::karma::double_;
+        using boost::spirit::karma::ascii::string;
+        using boost::phoenix::ref;
+        //]
+
+        //[reference_karma_and_predicate
+        boost::variant<double, int, std::string> v(1.0);
+        test_generator("numeric: 1.0", 
+                &((double_ | int_)[_1 = ref(v)])      << "numeric: " << double_
+            |   &((string | eps(false))[_1 = ref(v)]) << "string: "  << string
+        );
+
+        v = "example"
+        test_generator("string: example", 
+                &((double_ | int_)[_1 = ref(v)])      << "numeric: " << double_
+            |   &((string | eps(false))[_1 = ref(v)]) << "string: "  << string
+        );
+        //]
+    }
+
+    {
+        //[reference_karma_using_declarations_not_predicate
+        using boost::spirit::karma::generate;
+        using boost::spirit::karma::double_;
+        using boost::spirit::karma::ascii::string;
+        using boost::phoenix::ref;
+        //]
+
+        //[reference_karma_not_predicate
+        boost::variant<double, int, std::string> v(1.0);
+        test_generator("not a string!", 
+                !((double_ | int_)[_1 = ref(v)])      << "not numeric!"
+            |   !((string | eps(false))[_1 = ref(v)]) << "not a string!"
+        );
+
+        v = "example"
+        test_generator("not numeric!", 
+                !((double_ | int_)[_1 = ref(v)])      << "not numeric!"
+            |   !((string | eps(false))[_1 = ref(v)]) << "not a string!"
+        );
+        //]
+    }
+
     return 0;
 }
