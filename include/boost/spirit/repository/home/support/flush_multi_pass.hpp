bf1b29133c0f0eaaf545fb5be0adc4db3fac3ddd
François Barel <frabar666@gmail.com>
2009-08-12 20:55:05 +0000

    Spirit: fix broken include guard
    [SVN r55554]

--- include/boost/spirit/repository/home/support/flush_multi_pass.hpp
+++ include/boost/spirit/repository/home/support/flush_multi_pass.hpp
@@ -3,8 +3,8 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#if !defined(BOOST_SPIRIT_REPOSITORY_SUPPORT_CONFIX_APR_28_2009_0110PM)
-#define BOOST_SPIRIT_REPOSITORY_SUPPORT_CONFIX_APR_28_2009_0110PM
+#if !defined(BOOST_SPIRIT_REPOSITORY_SUPPORT_FLUSH_MULTI_PASS_JUL_11_2009_0823PM)
+#define BOOST_SPIRIT_REPOSITORY_SUPPORT_FLUSH_MULTI_PASS_JUL_11_2009_0823PM
 
 #if defined(_MSC_VER)
 #pragma once
