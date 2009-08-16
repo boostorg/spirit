e761bb55e1c07e252c0b1a92333cd30f72547830
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2009-08-16 22:54:12 +0000

    Spirit: doc update (added doc for repeat[] directive, minor additions and corrections all over the place)
    [SVN r55617]

--- example/karma/reference.cpp
+++ example/karma/reference.cpp
@@ -205,6 +205,27 @@ main()
     }
 
     {
+        //[reference_karma_using_declarations_repeat
+        using boost::spirit::karma::generate;
+        using boost::spirit::karma::double_;
+        using boost::spirit::karma::repeat;
+        //]
+
+        //[reference_karma_repeat
+        std::vector<double> v;
+        v.push_back(1.0);
+        v.push_back(2.0);
+        v.push_back(3.0);
+
+        test_generator_attr("[1.0][2.0][3.0]", repeat['[' << double_ << ']'], v);
+        test_generator_attr("[1.0][2.0]", repeat(2)['[' << double_ << ']'], v);
+
+        // fails because of insufficient number of items
+        test_generator_attr("", repeat(4)['[' << double_ << ']'], v);
+        //]
+    }
+
+    {
         //[reference_karma_using_declarations_buffer
         using boost::spirit::karma::generate;
         using boost::spirit::karma::double_;
