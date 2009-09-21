ba1ea3327e6589425d7711e61c2786e17364ccc9
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2009-09-21 17:54:33 +0000

    Spirit: silencing a warning
    [SVN r56343]

--- include/boost/spirit/home/support/char_encoding/standard_wide.hpp
+++ include/boost/spirit/home/support/char_encoding/standard_wide.hpp
@@ -60,10 +60,15 @@ namespace boost { namespace spirit { namespace char_encoding
         static bool
         ischar(int ch)
         {
-            // we have to watch out for sign extensions
-            return ( 0 == (ch & ~traits::wchar_t_size<sizeof(wchar_t)>::mask) || 
-                    ~0 == (ch | traits::wchar_t_size<sizeof(wchar_t)>::mask)) ? 
-                true : false;     // any wchar_t, but no other bits set
+            // we have to watch out for sign extensions (casting is there to 
+            // silence certain compilers complaining about signed/unsigned
+            // mismatch)
+            return (
+                std::size_t(0) == 
+                    std::size_t(ch & ~traits::wchar_t_size<sizeof(wchar_t)>::mask) || 
+                std::size_t(~0) == 
+                    std::size_t(ch | traits::wchar_t_size<sizeof(wchar_t)>::mask)
+            ) ? true : false;     // any wchar_t, but no other bits set
         }
 
         static bool
