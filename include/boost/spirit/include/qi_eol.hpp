22af1b3dc1c54e85f2dce94ef65d7b5392901fe7
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2009-10-28 21:06:28 +0000

    Spirit: merging from trunk
    [SVN r57201]

--- include/boost/spirit/include/qi_eol.hpp
+++ include/boost/spirit/include/qi_eol.hpp
@@ -6,13 +6,13 @@
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 =============================================================================*/
-#ifndef BOOST_SPIRIT_INCLUDE_QI_SKIP
-#define BOOST_SPIRIT_INCLUDE_QI_SKIP
+#ifndef BOOST_SPIRIT_INCLUDE_QI_EOL
+#define BOOST_SPIRIT_INCLUDE_QI_EOL
 
 #if defined(_MSC_VER)
 #pragma once
 #endif
 
-#include <boost/spirit/home/qi/directive/skip.hpp>
+#include <boost/spirit/home/qi/auxiliary/eol.hpp>
 
 #endif
--- include/boost/spirit/include/qi_symbols.hpp
+++ include/boost/spirit/include/qi_symbols.hpp
@@ -6,13 +6,13 @@
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 =============================================================================*/
-#ifndef BOOST_SPIRIT_INCLUDE_QI_SKIP
-#define BOOST_SPIRIT_INCLUDE_QI_SKIP
+#ifndef BOOST_SPIRIT_INCLUDE_QI_SYMBOLS
+#define BOOST_SPIRIT_INCLUDE_QI_SYMBOLS
 
 #if defined(_MSC_VER)
 #pragma once
 #endif
 
-#include <boost/spirit/home/qi/directive/skip.hpp>
+#include <boost/spirit/home/qi/string/symbols.hpp>
 
 #endif
