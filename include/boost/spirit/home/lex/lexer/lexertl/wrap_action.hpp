fc36606d1d774f4bac802cbda6a0eb897a3855f0
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2009-11-03 01:53:46 +0000

    Spirit: fixing usage of plain functions as lexer semantic actions
    [SVN r57298]

--- include/boost/spirit/home/lex/lexer/lexertl/wrap_action.hpp
+++ include/boost/spirit/home/lex/lexer/lexertl/wrap_action.hpp
@@ -74,17 +74,8 @@ namespace boost { namespace spirit { namespace lex { namespace lexertl
                 f(start, end, pass, id);
             }
 
-            template <typename F>
-            static void arg4_action_bool(F* f, Iterator& start, Iterator& end
-              , BOOST_SCOPED_ENUM(pass_flags)& pass, IdType& id, Context const&)
-            {
-                bool pass_bool = true;
-                f (start, end, pass_bool, id);
-                pass = pass_bool ? pass_flags::pass_normal : pass_flags::pass_fail;
-            }
-
             template <typename A0, typename A1, typename A2, typename A3>
-            static FunctionType call(void(*f)(A0, A1, A2, A3))
+            static FunctionType call(void (*f)(A0, A1, A2, A3))
             {
                 void (*pf)(void(*)(A0, A1, A2, A3)
                   , Iterator&, Iterator&, BOOST_SCOPED_ENUM(pass_flags)&
@@ -94,21 +85,8 @@ namespace boost { namespace spirit { namespace lex { namespace lexertl
                 using phoenix::arg_names::_2;
                 using phoenix::arg_names::_3;
                 using phoenix::arg_names::_4;
-                return phoenix::bind(pf, f, _1, _2, _3, _4);
-            }
-
-            template <typename A0, typename A1, typename A3>
-            static FunctionType call(void(*f)(A0, A1, bool&, A3))
-            {
-                void (*pf)(void(*)(A0, A1, bool&, A3), Iterator&, Iterator&
-                  , BOOST_SCOPED_ENUM(pass_flags)&, IdType&, Context const&) 
-                      = &wrap_action::arg4_action_bool;
-
-                using phoenix::arg_names::_1;
-                using phoenix::arg_names::_2;
-                using phoenix::arg_names::_3;
-                using phoenix::arg_names::_4;
-                return phoenix::bind(pf, f, _1, _2, _3, _4);
+                using phoenix::arg_names::_5;
+                return phoenix::bind(pf, f, _1, _2, _3, _4, _5);
             }
 
             // semantic actions with 3 arguments
@@ -120,17 +98,8 @@ namespace boost { namespace spirit { namespace lex { namespace lexertl
                 f(start, end, pass);
             }
 
-            template <typename F>
-            static void arg3_action_bool(F* f, Iterator& start, Iterator& end
-              , BOOST_SCOPED_ENUM(pass_flags)& pass, IdType, Context const&)
-            {
-                bool pass_bool = true;
-                f (start, end, pass_bool);
-                pass = pass_bool ? pass_flags::pass_normal : pass_flags::pass_fail;
-            }
-
             template <typename A0, typename A1, typename A2>
-            static FunctionType call(void(*f)(A0, A1, A2))
+            static FunctionType call(void (*f)(A0, A1, A2))
             {
                 void (*pf)(void(*)(A0, A1, A2), Iterator&, Iterator&
                   , BOOST_SCOPED_ENUM(pass_flags)&, IdType
@@ -139,39 +108,32 @@ namespace boost { namespace spirit { namespace lex { namespace lexertl
                 using phoenix::arg_names::_1;
                 using phoenix::arg_names::_2;
                 using phoenix::arg_names::_3;
-                return phoenix::bind(pf, f, _1, _2, _3);
-            }
-
-            template <typename A0, typename A1>
-            static FunctionType call(void(*f)(A0, A1, bool&))
-            {
-                void (*pf)(void(*)(A0, A1, bool&), Iterator&, Iterator&
-                  , BOOST_SCOPED_ENUM(pass_flags)&, IdType, Context const&) 
-                      = &wrap_action::arg3_action_bool;
-
-                using phoenix::arg_names::_1;
-                using phoenix::arg_names::_2;
-                using phoenix::arg_names::_3;
-                return phoenix::bind(pf, f, _1, _2, _3);
+                using phoenix::arg_names::_4;
+                using phoenix::arg_names::_5;
+                return phoenix::bind(pf, f, _1, _2, _3, _4, _5);
             }
 
             // semantic actions with 2 arguments
             template <typename F>
             static void arg2_action(F* f, Iterator& start, Iterator& end
-              , bool, IdType, Context const&)
+              , BOOST_SCOPED_ENUM(pass_flags)&, IdType, Context const&)
             {
-                f(start, end);
+                f (start, end);
             }
 
             template <typename A0, typename A1>
-            static FunctionType call(void(*f)(A0, A1))
+            static FunctionType call(void (*f)(A0, A1))
             {
-                void (*pf)(void(*)(A0, A1), Iterator&, Iterator&, bool
+                void (*pf)(void(*)(A0, A1), Iterator&, Iterator&
+                  , BOOST_SCOPED_ENUM(pass_flags)&
                   , IdType, Context const&) = &wrap_action::arg2_action;
 
                 using phoenix::arg_names::_1;
                 using phoenix::arg_names::_2;
-                return phoenix::bind(pf, f, _1, _2);
+                using phoenix::arg_names::_3;
+                using phoenix::arg_names::_4;
+                using phoenix::arg_names::_5;
+                return phoenix::bind(pf, f, _1, _2, _3, _4, _5);
             }
 
             // we assume that either both iterators are to be passed to the 
@@ -181,17 +143,23 @@ namespace boost { namespace spirit { namespace lex { namespace lexertl
             // semantic actions with 0 argument
             template <typename F>
             static void arg0_action(F* f, Iterator&, Iterator&
-              , bool, IdType, Context const&)
+              , BOOST_SCOPED_ENUM(pass_flags)&, IdType, Context const&)
             {
                 f();
             }
 
-            static FunctionType call(void(*f)())
+            static FunctionType call(void (*f)())
             {
-                void (*pf)(void(*)(), Iterator&, Iterator&, bool
+                void (*pf)(void(*)(), Iterator&, Iterator&
+                  , BOOST_SCOPED_ENUM(pass_flags)&
                   , IdType, Context const&) = &arg0_action;
 
-                return phoenix::bind(pf, f);
+                using phoenix::arg_names::_1;
+                using phoenix::arg_names::_2;
+                using phoenix::arg_names::_3;
+                using phoenix::arg_names::_4;
+                using phoenix::arg_names::_5;
+                return phoenix::bind(pf, f, _1, _2, _3, _4, _5);
             }
         };
 
@@ -200,7 +168,7 @@ namespace boost { namespace spirit { namespace lex { namespace lexertl
         template <typename Iterator, typename Context, typename Idtype>
         struct wrap_action<unused_type, Iterator, Context, Idtype>
         {
-            // plain functors are not touched at all
+            // plain function objects are not touched at all
             template <typename F>
             static F const& call(F const& f)
             {
