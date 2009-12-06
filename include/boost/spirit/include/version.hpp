27ff19ba5a4cc67a8a8322fe234d07376194068e
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2009-12-06 18:52:57 +0000

    Spirit: fixed include guard conflict (fixes #3724)
    [SVN r58196]

--- include/boost/spirit/include/version.hpp
+++ include/boost/spirit/include/version.hpp
@@ -6,8 +6,8 @@
   Distributed under the Boost Software License, Version 1.0. (See accompanying
   file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 =============================================================================*/
-#if !defined(SPIRIT_VERSION_NOVEMBER_13_2008_0834AM)
-#define SPIRIT_VERSION_NOVEMBER_13_2008_0834AM
+#if !defined(SPIRIT_VERSION)
+#define SPIRIT_VERSION
 
 ///////////////////////////////////////////////////////////////////////////////
 //
