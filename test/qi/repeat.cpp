e774599d93c0699d3fbd3c07c44a994dd2d53b29
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2009-12-16 12:31:16 +0000

    Spirit: fixing qi::repeat[] for unused attributes, augmented tests
    [SVN r58414]

--- test/qi/repeat.cpp
+++ test/qi/repeat.cpp
@@ -157,6 +157,11 @@ main()
     }
 
     {
+        BOOST_TEST(test("1 2 3", int_ >> repeat(2)[int_], space));
+        BOOST_TEST(!test("1 2", int_ >> repeat(2)[int_], space));
+    }
+
+    {
         std::vector<char> v;
         BOOST_TEST(test_attr("1 2 3", int_ >> repeat(2)[int_], v, space));
         BOOST_TEST(v.size() == 3 && v[0] == 1 && v[1] == 2 && v[2] == 3);
