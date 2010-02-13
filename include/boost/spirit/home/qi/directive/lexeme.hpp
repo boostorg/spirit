f33faca44e737779c449a32f370ba25533a9d820
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2010-02-13 00:21:29 +0000

    Spirit: raw[] and lexeme.hpp[] do not pre-skip anymore
    [SVN r59662]

--- include/boost/spirit/home/qi/directive/lexeme.hpp
+++ include/boost/spirit/home/qi/directive/lexeme.hpp
@@ -56,7 +56,6 @@ namespace boost { namespace spirit { namespace qi
           , Context& context, Skipper const& skipper
           , Attribute& attr) const
         {
-            qi::skip_over(first, last, skipper);
             return subject.parse(first, last, context
               , detail::unused_skipper<Skipper>(skipper), attr);
         }
