907ba3bc07435b8308c49d265ee3e2ab47ca1b5f
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2010-03-20 21:20:37 +0000

    Spirit: Fixing bug in display_attribute_of_parser utility.
    [SVN r60738]

--- example/qi/display_attribute_type.hpp
+++ example/qi/display_attribute_type.hpp
@@ -33,6 +33,11 @@ namespace tools
     template <typename T>
     void display_attribute_of_parser(T const &) 
     {
+        // Report invalid expression error as early as possible.
+        // If you got an error_invalid_expression error message here,
+        // then the expression (expr) is not a valid spirit qi expression.
+        BOOST_SPIRIT_ASSERT_MATCH(qi::domain, T);
+
         typedef typename attribute_of_parser<T>::type type;
         std::cout << typeid(type).name() << std::endl;
     }
@@ -40,6 +45,11 @@ namespace tools
     template <typename T>
     void display_attribute_of_parser(std::ostream& os, T const &) 
     {
+        // Report invalid expression error as early as possible.
+        // If you got an error_invalid_expression error message here,
+        // then the expression (expr) is not a valid spirit qi expression.
+        BOOST_SPIRIT_ASSERT_MATCH(qi::domain, T);
+
         typedef typename attribute_of_parser<T>::type type;
         os << typeid(type).name() << std::endl;
     }
