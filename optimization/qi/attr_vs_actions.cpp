669add0fc201493ba449a89b70b377815e69d81c
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2010-03-26 13:29:48 +0000

    Spirit: Fixed copyrights
    [SVN r60860]

--- optimization/qi/attr_vs_actions.cpp
+++ optimization/qi/attr_vs_actions.cpp
@@ -1,3 +1,8 @@
+//   Copyright (c) 2002-2010 Joel de Guzman
+// 
+//   Distributed under the Boost Software License, Version 1.0. (See accompanying
+//   file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #include <boost/config/warning_disable.hpp>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/phoenix_core.hpp>
