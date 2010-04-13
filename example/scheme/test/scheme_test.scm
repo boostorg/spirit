42ae3766ede8863cbdcd9b05c94ae8ab5629d5d8
Joel de Guzman <djowel@gmail.com>
2010-04-13 16:09:59 +0000

    + lambda working + fix to sexpr symbol matching + workaround to iterator_range bug
    [SVN r61250]

--- example/scheme/test/scheme_test.scm
+++ example/scheme/test/scheme_test.scm
@@ -1,3 +1,6 @@
+; These tests demostrate the functionality of the scheme
+; compiler/interpreter
+
 (define (dbl x) (+ x x))
 
 (define len 123)
@@ -6,8 +9,8 @@
 
 ; The hello-world for interpreters ;-)
 (define (factorial n)
-    (if (<= n 0) 1
-        (* n (factorial (- n 1)))))
+  (if (<= n 0) 1
+    (* n (factorial (- n 1)))))
 
 (factorial 10) ; 3628800
 
@@ -18,4 +21,4 @@
         n
         (+ (fib (- n 1)) (fib (- n 2))))))
 
-(fib 10)
+(fib 10) ; 55
