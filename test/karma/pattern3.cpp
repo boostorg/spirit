7a2557646c803170ccc1f4897597441ba0b19820
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2010-04-18 18:23:18 +0000

    Spirit: fixing inspect errors
    [SVN r61371]

--- test/karma/pattern3.cpp
+++ test/karma/pattern3.cpp
@@ -3,6 +3,9 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+// this file deliberately contains non-ascii characters
+// boostinspect:noascii
+
 #include <boost/config/warning_disable.hpp>
 #include <boost/detail/lightweight_test.hpp>
 
--- test/qi/rule.cpp
+++ test/qi/rule.cpp
@@ -4,6 +4,10 @@
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 =============================================================================*/
+
+// this file deliberately contains non-ascii characters
+// boostinspect:noascii
+
 #include <boost/detail/lightweight_test.hpp>
 #include <boost/spirit/include/qi_operator.hpp>
 #include <boost/spirit/include/qi_char.hpp>
