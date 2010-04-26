fb3b60db47226d5f78a558f6cb46502c12592129
Joel de Guzman <djowel@gmail.com>
2010-04-26 13:02:30 +0000

    lists of ints grammar almost working
    [SVN r61577]

--- example/scheme/test/qi/calc.scm
+++ example/scheme/test/qi/calc.scm
@@ -8,3 +8,26 @@
             (qi:|
                 (qi:>> (qi:char_ "+") (qi:int_))
                 (qi:>> (qi:char_ "-") (qi:int_))))))
+
+; (define expression)
+
+; (define factor
+    ; (qi:|
+        ; (qi:int_)
+        ; (qi:>> (qi:char_ "(") (expression) (qi:char_ ")"))
+        ; (qi:>> (qi:char_ "-") (factor))
+        ; (qi:>> (qi:char_ "+") (factor))))
+
+; (define term
+    ; (qi:>> (factor)
+        ; (qi:*
+            ; (qi:|
+                ; (qi:>> (qi:char_ "*") (factor))
+                ; (qi:>> (qi:char_ "/") (factor))))))
+
+; (define expression
+    ; (qi:>> (term)
+        ; (qi:*
+            ; (qi:|
+                ; (qi:>> (qi:char_ "+") (term))
+                ; (qi:>> (qi:char_ "-") (term))))))
