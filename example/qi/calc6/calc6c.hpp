83a792d7ede550d0387459eedc640438249c7716
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2011-01-03 16:58:38 +0000

    Spirit: updating copyrights
    [SVN r67619]

--- example/qi/calc6/calc6c.hpp
+++ example/qi/calc6/calc6c.hpp
@@ -1,5 +1,5 @@
 /*=============================================================================
-    Copyright (c) 2001-2010 Joel de Guzman
+    Copyright (c) 2001-2011 Joel de Guzman
 
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -43,11 +43,11 @@ statement<Iterator>::statement(std::vector<int>& code)
         ;
 
     var_decl =
-		lexeme[
-			"var"
-			>>  !(alnum | '_')      // make sure we have whole words
-		]         
-		>   !var_ref                // make sure the variable isn't redeclared
+        lexeme[
+            "var"
+            >>  !(alnum | '_')      // make sure we have whole words
+        ]         
+        >   !var_ref                // make sure the variable isn't redeclared
         >   identifier      [add_var(_1, ref(nvars))]
         >   (';' | '=' > assignment_rhs(ref(nvars)-1))
         ;
