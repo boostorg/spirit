babee8bef07ffa247706f96e43db675d8795a6a2
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2011-01-19 23:45:38 +0000

    Spirit: merging docs from trunk, merging tests for fix for proto/fusion/spirit issue
    [SVN r68310]

--- phoenix/example/users_manual/all_odds.cpp
+++ phoenix/example/users_manual/all_odds.cpp
@@ -1,7 +1,7 @@
 /*=============================================================================
     Copyright (c) 2001-2007 Joel de Guzman
 
-    Distributed under the Boost Software License, Version 1.0. (See accompanying 
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
 #include <vector>
@@ -25,7 +25,7 @@ main()
 
     //  Print all odd contents of an stl container c
     for_each(c.begin(), c.end(),
-        if_(arg1 % 2 == 1)
+        if_(arg1 % 2 != 0)
         [
             cout << arg1 << ' '
         ]
