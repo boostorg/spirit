8cf21d42cd9d0a1559178e786c1280c66d42def6
Thomas Heller <thom.heller@googlemail.com>
2011-04-30 14:41:51 +0000

    [spirit] fixed issue with phoenix V3 and no predefined terminals
    [SVN r71624]

--- include/boost/spirit/home/support/context.hpp
+++ include/boost/spirit/home/support/context.hpp
@@ -230,47 +230,47 @@ namespace boost { namespace spirit
         }
     };
     
-    typedef phoenix::actor<attribute<0> > const _val_type;
-    typedef phoenix::actor<attribute<0> > const _r0_type;
-    typedef phoenix::actor<attribute<1> > const _r1_type;
-    typedef phoenix::actor<attribute<2> > const _r2_type;
+    typedef phoenix::actor<attribute<0> > _val_type;
+    typedef phoenix::actor<attribute<0> > _r0_type;
+    typedef phoenix::actor<attribute<1> > _r1_type;
+    typedef phoenix::actor<attribute<2> > _r2_type;
 
 #ifndef BOOST_SPIRIT_NO_PREDEFINED_TERMINALS
     // _val refers to the 'return' value of a rule (same as _r0)
     // _r1, _r2, ... refer to the rule arguments
-    _val_type _val = _val_type();
-    _r0_type _r0 = _r0_type();
-    _r1_type _r1 = _r1_type();
-    _r2_type _r2 = _r2_type();
+    _val_type const _val = _val_type();
+    _r0_type const _r0 = _r0_type();
+    _r1_type const _r1 = _r1_type();
+    _r2_type const _r2 = _r2_type();
 #endif
 
     //  Bring in the rest of the attributes (_r4 .. _rN+1), using PP
     BOOST_PP_REPEAT_FROM_TO(
         3, SPIRIT_ATTRIBUTES_LIMIT, SPIRIT_DECLARE_ATTRIBUTE, _)
 
-    typedef phoenix::actor<local_variable<0> > const _a_type;
-    typedef phoenix::actor<local_variable<1> > const _b_type;
-    typedef phoenix::actor<local_variable<2> > const _c_type;
-    typedef phoenix::actor<local_variable<3> > const _d_type;
-    typedef phoenix::actor<local_variable<4> > const _e_type;
-    typedef phoenix::actor<local_variable<5> > const _f_type;
-    typedef phoenix::actor<local_variable<6> > const _g_type;
-    typedef phoenix::actor<local_variable<7> > const _h_type;
-    typedef phoenix::actor<local_variable<8> > const _i_type;
-    typedef phoenix::actor<local_variable<9> > const _j_type;
+    typedef phoenix::actor<local_variable<0> > _a_type;
+    typedef phoenix::actor<local_variable<1> > _b_type;
+    typedef phoenix::actor<local_variable<2> > _c_type;
+    typedef phoenix::actor<local_variable<3> > _d_type;
+    typedef phoenix::actor<local_variable<4> > _e_type;
+    typedef phoenix::actor<local_variable<5> > _f_type;
+    typedef phoenix::actor<local_variable<6> > _g_type;
+    typedef phoenix::actor<local_variable<7> > _h_type;
+    typedef phoenix::actor<local_variable<8> > _i_type;
+    typedef phoenix::actor<local_variable<9> > _j_type;
 
 #ifndef BOOST_SPIRIT_NO_PREDEFINED_TERMINALS
     // _a, _b, ... refer to the local variables of a rule
-    _a_type _a = _a_type();
-    _b_type _b = _b_type();
-    _c_type _c = _c_type();
-    _d_type _d = _d_type();
-    _e_type _e = _e_type();
-    _f_type _f = _f_type();
-    _g_type _g = _g_type();
-    _h_type _h = _h_type();
-    _i_type _i = _i_type();
-    _j_type _j = _j_type();
+    _a_type const _a = _a_type();
+    _b_type const _b = _b_type();
+    _c_type const _c = _c_type();
+    _d_type const _d = _d_type();
+    _e_type const _e = _e_type();
+    _f_type const _f = _f_type();
+    _g_type const _g = _g_type();
+    _h_type const _h = _h_type();
+    _i_type const _i = _i_type();
+    _j_type const _j = _j_type();
 #endif
 
     // You can bring these in with the using directive
