d97bf6b521d74fc3585f0d548f9365a90b641619
Joel de Guzman <djowel@gmail.com>
2011-11-05 09:56:29 +0000

    Docs for the seek directive
    [SVN r75321]

--- repository/example/qi/seek.cpp
+++ repository/example/qi/seek.cpp
@@ -5,15 +5,13 @@
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //////////////////////////////////////////////////////////////////////////////*/
 
-//  [ Jamboree May 3, 2011 ]        first ver.
-//  [ Jamboree Aug 13, 2011 ]       minor change.
+//  [ Jamboree Oct 27, 2011 ]       new example.
 
 
 #include <cstdlib>
 #include <iostream>
 
 #include <boost/spirit/include/qi.hpp>
-#include <boost/spirit/include/karma.hpp>
 #include <boost/spirit/repository/include/qi_seek.hpp>
 
 
@@ -21,36 +19,30 @@ int main(int argc, char *argv[])
 {
     //[reference_qi_seek_namespace
     namespace qi = boost::spirit::qi;
-    namespace ka = boost::spirit::karma;
     namespace repo = boost::spirit::repository;
     //]
-
-    std::string line;
+    
     typedef std::string::const_iterator iterator;
 
-    while (std::cout << ">>> ", std::getline(std::cin, line))
-    {
-        //[reference_qi_seek_vars
-        iterator it = line.begin();
-        iterator end = line.end();
-        std::vector<int> val;
-        //]
+    //[reference_qi_seek_vars
+    std::string str("/*C-style comment*/");
+    iterator it = str.begin();
+    iterator end = str.end();
+    //]
 
-        //[reference_qi_seek_parse
-        if (qi::phrase_parse(it, end, +repo::qi::seek["#" >> qi::int_], qi::space, val))
-        {
-            std::cout << "-------------------------------- \n";
-            std::cout << "Parsing succeeded, got: "
-                      << ka::format("[ " << ka::int_ % ", " << " ]\n", val);
-            std::cout << "---------------------------------\n";
-        }//]
-        else
-        {
-            std::cout << "-------------------------------- \n";
-            std::cout << "Parsing failed, rest: " << std::string(it, end) << "\n";
-            std::cout << "-------------------------------- \n";
-        }
+    //[reference_qi_seek_parse
+    if (qi::parse(it, end, "/*" >> repo::qi::seek["*/"]))
+    {
+        std::cout << "-------------------------------- \n";
+        std::cout << "Parsing succeeded.\n";
+        std::cout << "---------------------------------\n";
     }
+    else
+    {
+        std::cout << "-------------------------------- \n";
+        std::cout << "Unterminated /* comment.\n";
+        std::cout << "-------------------------------- \n";
+    }//]
 
     return EXIT_SUCCESS;
 }
