12a7f86ca8e40319892a2b7e8a64030e91aa6b48
Joel de Guzman <djowel@gmail.com>
2013-01-07 22:00:23 +0800

    more kleene tests

--- libs/spirit/test/x3/kleene.cpp
+++ libs/spirit/test/x3/kleene.cpp
@@ -10,34 +10,35 @@
 #include <boost/detail/lightweight_test.hpp>
 
 #include <boost/spirit/home/x3/operator/kleene.hpp>
+#include <boost/spirit/home/x3/numeric.hpp>
 #include <boost/spirit/home/x3/char.hpp>
 
 #include <string>
 #include <iostream>
 #include "test.hpp"
 
-//~ struct x_attr
-//~ {
-//~ };
+struct x_attr
+{
+};
 
-//~ namespace boost { namespace spirit { namespace traits
-//~ {
-    //~ template <>
-    //~ struct container_value<x_attr>
-    //~ {
-        //~ typedef char type; // value type of container
-    //~ };
+namespace boost { namespace spirit { namespace traits
+{
+    template <>
+    struct container_value<x_attr>
+    {
+        typedef char type; // value type of container
+    };
 
-    //~ template <>
-    //~ struct push_back_container<x_attr, char>
-    //~ {
-        //~ static bool call(x_attr& /*c*/, char /*val*/)
-        //~ {
-            //~ // push back value type into container
-            //~ return true;
-        //~ }
-    //~ };
-//~ }}}
+    template <>
+    struct push_back_container<x_attr, char>
+    {
+        static bool call(x_attr& /*c*/, char /*val*/)
+        {
+            // push back value type into container
+            return true;
+        }
+    };
+}}}
 
 int
 main()
@@ -52,7 +53,7 @@ main()
     using boost::spirit::x3::digit;
     //~ using boost::spirit::x3::no_case;
     //~ using boost::spirit::x3::uint_;
-    //~ using boost::spirit::x3::int_;
+    using boost::spirit::x3::int_;
     //~ using boost::spirit::x3::lexeme;
 
     {
@@ -67,16 +68,16 @@ main()
         BOOST_TEST(test(" a a aaa aa", *char_, space));
         BOOST_TEST(test("12345 678 9", *digit, space));
     }
-/*
-    {
-        BOOST_TEST(test("aBcdeFGH", no_case[*char_]));
-        BOOST_TEST(test("a B cde FGH", no_case[*char_], space));
-    }
 
-    {
-        BOOST_TEST(test("12345 678 955 987", *uint_, space));
-        BOOST_TEST(test("12345, 678, 955, 987", uint_ >> *(',' >> uint_), space));
-    }
+    //~ {
+        //~ BOOST_TEST(test("aBcdeFGH", no_case[*char_]));
+        //~ BOOST_TEST(test("a B cde FGH", no_case[*char_], space));
+    //~ }
+
+    //~ {
+        //~ BOOST_TEST(test("12345 678 955 987", *uint_, space));
+        //~ BOOST_TEST(test("12345, 678, 955, 987", uint_ >> *(',' >> uint_), space));
+    //~ }
 
     {
         std::string s;
@@ -99,18 +100,18 @@ main()
             v[0] == 123 && v[1] == 456 && v[2] == 789 &&  v[3] == 10);
     }
 
-    {
-        std::vector<std::string> v;
-        BOOST_TEST(test_attr("a b c d", *lexeme[+alpha], v, space) && 4 == v.size() &&
-            v[0] == "a" && v[1] == "b" && v[2] == "c" &&  v[3] == "d");
-    }
+    //~ {
+        //~ std::vector<std::string> v;
+        //~ BOOST_TEST(test_attr("a b c d", *lexeme[+alpha], v, space) && 4 == v.size() &&
+            //~ v[0] == "a" && v[1] == "b" && v[2] == "c" &&  v[3] == "d");
+    //~ }
 
     {
         std::vector<int> v;
         BOOST_TEST(test_attr("123 456 789", *int_, v, space) && 3 == v.size() &&
             v[0] == 123 && v[1] == 456 && v[2] == 789);
     }
-
+/*
     { // actions
         namespace phx = boost::phoenix;
         using boost::spirit::_1;
@@ -128,13 +129,13 @@ main()
         BOOST_TEST(test("123 456 789", (*int_)[phx::ref(v) = _1], space) && 3 == v.size() &&
             v[0] == 123 && v[1] == 456 && v[2] == 789);
     }
-
+*/
     { // attribute customization
 
         x_attr x;
         test_attr("abcde", *char_, x);
     }
-*/
+
     return boost::report_errors();
 }
 
