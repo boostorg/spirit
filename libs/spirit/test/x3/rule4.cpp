b28452f4ddeda693740ac1e66c0d08027490c529
K-ballo <k@fusionfenix.com>
2013-02-28 22:48:21 -0300

    Added rule attribute compatibility test
    Signed-off-by: K-ballo <k@fusionfenix.com>

--- libs/spirit/test/x3/rule4.cpp
+++ libs/spirit/test/x3/rule4.cpp
@@ -203,6 +203,24 @@ main()
         //~ std::cout << attr << std::endl;
     //~ }
 
+    { // attribute compatibility test
+        using boost::spirit::x3::grammar;
+        using boost::spirit::x3::rule;
+        using boost::spirit::x3::int_;
+
+        auto const expr = int_;
+        
+        short i;
+        BOOST_TEST(test_attr("1", expr, i) && i == 1); // ok
+
+        const rule< class int_rule, int > int_rule( "int_rule" );
+        auto const int_rule_def = int_;
+        auto const g = grammar( "g", int_rule = int_rule_def );
+        
+        short j;
+        BOOST_TEST(test_attr("1", g, j) && j == 1); // error
+    }
+
     return boost::report_errors();
 }
 
