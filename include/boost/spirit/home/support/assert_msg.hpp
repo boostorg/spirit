892d8ec12597a049c1fa3c6b550532d5bb4be3df
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-04-06 14:36:38 +0000

    Fix #8381: karma::uint_generator fails to compile in C++11 mode
    [SVN r83776]

--- include/boost/spirit/home/support/assert_msg.hpp
+++ include/boost/spirit/home/support/assert_msg.hpp
@@ -33,8 +33,22 @@
 
 #define BOOST_SPIRIT_ASSERT_MATCH(Domain, Expr)                               \
         BOOST_SPIRIT_ASSERT_MSG((                                             \
-            boost::spirit::traits::matches<Domain, Expr>::value               \
+            boost::spirit::traits::matches< Domain, Expr >::value             \
         ), error_invalid_expression, (Expr))
+
+// GCC 4.7 will overeagerly instantiate static_asserts in template functions,
+// if the assert condition does not depend on template parameters
+// (see https://svn.boost.org/trac/boost/ticket/8381).
+// There are places where we want to use constant false as the condition in
+// template functions to indicate that these function overloads should never
+// be called. This allows to generate better error messages. To solve this
+// problem we make the condition dependent on the template argument and use
+// the following macro in such places.
+#include <boost/type_traits/is_same.hpp>
+
+#define BOOST_SPIRIT_ASSERT_FAIL(TemplateParam, Msg, Types)                   \
+        BOOST_SPIRIT_ASSERT_MSG((!boost::is_same<                             \
+            TemplateParam, TemplateParam >::value), Msg, Types)
 
 #endif
 
