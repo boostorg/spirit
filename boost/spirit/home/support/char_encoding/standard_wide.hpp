a0ed57c8ce29ae94fb86eb2f08db95154abcd248
K-ballo <k@fusionfenix.com>
2013-06-03 19:03:40 -0300

    Silence int to bool warnings (following standard_wide definitions)
    Signed-off-by: K-ballo <k@fusionfenix.com>

--- boost/spirit/home/support/char_encoding/standard_wide.hpp
+++ boost/spirit/home/support/char_encoding/standard_wide.hpp
@@ -153,6 +153,10 @@ namespace boost { namespace spirit { namespace char_encoding
             return (ch == L' ' || ch == L'\t');
         }
 
+        ///////////////////////////////////////////////////////////////////////
+        //  Simple character conversions
+        ///////////////////////////////////////////////////////////////////////
+
         static wchar_t
         tolower(wchar_t ch)
         {
