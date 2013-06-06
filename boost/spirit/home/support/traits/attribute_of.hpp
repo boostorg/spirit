5255ab41b99cb93aa02ee44c149f8279f4dbbf6c
K-ballo <k@fusionfenix.com>
2013-06-06 16:53:03 -0300

    Added support for a nested attribute metafunction as an alternative fallback for attribute_of trait
    Signed-off-by: K-ballo <k@fusionfenix.com>

--- boost/spirit/home/support/traits/attribute_of.hpp
+++ boost/spirit/home/support/traits/attribute_of.hpp
@@ -1,5 +1,6 @@
 /*=============================================================================
     Copyright (c) 2001-2013 Joel de Guzman
+    Copyright (c) 2013 Agustín Bergé
     http://spirit.sourceforge.net/
 
     Distributed under the Boost Software License, Version 1.0. (See accompanying
@@ -12,18 +13,37 @@
 #pragma once
 #endif
 
+#include <boost/spirit/home/support/utility/sfinae.hpp>
 #include <boost/mpl/identity.hpp>
 
 namespace boost { namespace spirit { namespace traits
 {
+    namespace detail
+    {
+        template <typename Component, typename Context, typename Enable = void>
+        struct default_attribute_of;
+        
+        template <typename Component, typename Context>
+        struct default_attribute_of<Component, Context, 
+            typename disable_if_substitution_failure<
+                typename Component::attribute_type>::type>
+          : mpl::identity<typename Component::attribute_type> {};
+        
+        template <typename Component, typename Context>
+        struct default_attribute_of<Component, Context, 
+            typename disable_if_substitution_failure<
+                typename Component::template attribute<Context>::type>::type>
+          : Component::template attribute<Context> {};
+    }
+
     ///////////////////////////////////////////////////////////////////////////
-    // Get the attribute type of a component. By default, this simply gets
-    // the Component's attribute_type typedef. Components may specialize
-    // this if such an attribute_type is not readily available (e.g. expensive
-    // to compute at compile time).
+    // Get the attribute type of a component. By default, this gets the 
+    // Component's attribute_type typedef or instantiates a nested attribute 
+    // metafunction. Components may specialize this if such an attribute_type
+    // is not readily available (e.g. expensive to compute at compile time).
     ///////////////////////////////////////////////////////////////////////////
     template <typename Component, typename Context, typename Enable = void>
-    struct attribute_of : mpl::identity<typename Component::attribute_type> {};
+    struct attribute_of : detail::default_attribute_of<Component, Context> {};
 
 }}}
 
