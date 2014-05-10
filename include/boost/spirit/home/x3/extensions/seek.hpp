3f0ec43f538c3abd1003be5bc2ad6608dd802217
Lee Clagett <code@leeclagett.com>
2014-05-10 15:19:23 -0400

    Modifying x3 seek implementation for better readability

--- include/boost/spirit/home/x3/extensions/seek.hpp
+++ include/boost/spirit/home/x3/extensions/seek.hpp
@@ -30,20 +30,25 @@ namespace boost { namespace spirit { namespace x3
         bool parse(Iterator& first, Iterator const& last
                    , Context const& context, Attribute& attr) const
         {
-            for (Iterator current(first);; ++current)
+            Iterator current(first);
+            for (/**/; current != last; ++current)
             {
                 if (this->subject.parse(current, last, context, attr))
                 {
                     first = current;
                     return true;
                 }
-
-                // fail only after subject fails & no input
-                if (current == last)
-                {
-                    return false;
-                }
             }
+
+            // Test for when subjects match on input empty. Example:
+            //     comment = "//" >> seek[eol | eoi]
+            if (this->subject.parse(current, last, context, attr))
+            {
+                first = current;
+                return true;
+            }
+
+            return false;
         }
     };
 
