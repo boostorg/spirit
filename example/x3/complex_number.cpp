d38949f6888a88f112b2cd6ad337151de34c6cff
Joel de Guzman <djowel@gmail.com>
2015-05-12 14:53:55 +0800

    complex example

--- example/x3/complex_number.cpp
+++ example/x3/complex_number.cpp
@@ -1,16 +1,15 @@
 /*=============================================================================
-    Copyright (c) 2002-2015 Joel de Guzman
+    Copyright (c) 2002-2010 Joel de Guzman
 
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 =============================================================================*/
 ///////////////////////////////////////////////////////////////////////////////
 //
-//  This sample demontrates a parser for a comma separated list of numbers.
-//  No actions.
+//  A complex number micro parser.
 //
 //  [ JDG May 10, 2002 ]    spirit1
-//  [ JDG March 24, 2007 ]  spirit2
+//  [ JDG May 9, 2007 ]     spirit2
 //  [ JDG May 12, 2015 ]    spirit X3
 //
 ///////////////////////////////////////////////////////////////////////////////
@@ -20,31 +19,41 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
+#include <complex>
 
+///////////////////////////////////////////////////////////////////////////////
+//  Our complex number parser/compiler
+///////////////////////////////////////////////////////////////////////////////
 namespace client
 {
-    namespace x3 = boost::spirit::x3;
-    namespace ascii = boost::spirit::x3::ascii;
-
-    ///////////////////////////////////////////////////////////////////////////
-    //  Our number list parser
-    ///////////////////////////////////////////////////////////////////////////
     template <typename Iterator>
-    bool parse_numbers(Iterator first, Iterator last)
+    bool parse_complex(Iterator first, Iterator last, std::complex<double>& c)
     {
-        using x3::double_;
-        using x3::phrase_parse;
-        using ascii::space;
+        using boost::spirit::x3::double_;
+        using boost::spirit::x3::_attr;
+        using boost::spirit::x3::phrase_parse;
+        using boost::spirit::x3::ascii::space;
 
-        bool r = phrase_parse(
-            first,                          // Start Iterator
-            last,                           // End Iterator
-            double_ >> *(',' >> double_),   // The Parser
-            space                           // The Skip-Parser
-        );
-        if (first != last) // fail if we did not get a full match
+        double rN = 0.0;
+        double iN = 0.0;
+        auto fr = [&](auto& ctx){ rN = _attr(ctx); };
+        auto fi = [&](auto& ctx){ iN = _attr(ctx); };
+
+        bool r = phrase_parse(first, last,
+
+            //  Begin grammar
+            (
+                    '(' >> double_[fr]
+                        >> -(',' >> double_[fi]) >> ')'
+                |   double_[fr]
+            ),
+            //  End grammar
+
+            space);
+
+        if (!r || first != last) // fail if we did not get a full match
             return false;
+        c = std::complex<double>(rN, iN);
         return r;
     }
 }
@@ -56,10 +65,10 @@ int
 main()
 {
     std::cout << "/////////////////////////////////////////////////////////\n\n";
-    std::cout << "\t\tA comma separated list parser for Spirit...\n\n";
+    std::cout << "\t\tA complex number micro parser for Spirit...\n\n";
     std::cout << "/////////////////////////////////////////////////////////\n\n";
 
-    std::cout << "Give me a comma separated list of numbers.\n";
+    std::cout << "Give me a complex number of the form r or (r) or (r,i) \n";
     std::cout << "Type [q or Q] to quit\n\n";
 
     std::string str;
@@ -68,11 +77,13 @@ main()
         if (str.empty() || str[0] == 'q' || str[0] == 'Q')
             break;
 
-        if (client::parse_numbers(str.begin(), str.end()))
+        std::complex<double> c;
+        if (client::parse_complex(str.begin(), str.end(), c))
         {
             std::cout << "-------------------------\n";
             std::cout << "Parsing succeeded\n";
-            std::cout << str << " Parses OK: " << std::endl;
+            std::cout << "got: " << c << std::endl;
+            std::cout << "\n-------------------------\n";
         }
         else
         {
