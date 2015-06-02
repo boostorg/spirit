b4036b75fd133309cf7e7654fe7bfb525f5da033
Mario Lang <mlang@delysid.org>
2015-06-02 22:09:29 +0200

    X3: Simplify lambda_visitor.

--- include/boost/spirit/home/x3/support/utility/lambda_visitor.hpp
+++ include/boost/spirit/home/x3/support/utility/lambda_visitor.hpp
@@ -7,36 +7,14 @@
 #if !defined(BOOST_SPIRIT_X3_LAMBDA_VISITOR_MAY_19_2014_1116AM)
 #define BOOST_SPIRIT_X3_LAMBDA_VISITOR_MAY_19_2014_1116AM
 
+#include <boost/variant/static_visitor.hpp>
+
 namespace boost { namespace spirit { namespace x3
 {
-    template <typename RT, typename... Lambdas>
-    struct lambda_visitor;
-
-    template <typename RT, typename F, typename... Lambdas>
-    struct lambda_visitor<RT, F, Lambdas...> : F, lambda_visitor<RT, Lambdas...>
+    template <typename RT, typename... F>
+    struct lambda_visitor : static_visitor<RT>, F...
     {
-        typedef lambda_visitor<RT , Lambdas...> base_type;
-        using F::operator();
-        using base_type::operator();
-        lambda_visitor(F f, Lambdas... lambdas)
-          : F(f), base_type(lambdas...)
-        {}
-    };
-
-    template <typename RT, typename F>
-    struct lambda_visitor<RT, F> : F
-    {
-        typedef RT result_type;
-        using F::operator();
-        lambda_visitor(F f)
-          : F(f)
-        {}
-    };
-
-    template <typename RT>
-    struct lambda_visitor<RT>
-    {
-        typedef RT result_type;
+        lambda_visitor(F... f) : F(f)... {}
     };
 
     template <typename RT, typename... Lambdas>
