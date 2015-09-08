boostorg/spirit

commit a4f9ea9d58303b0d6a4347bcea4f30a9fdddebd2

reenabled support for BOOST_FUSION_ADAPT_ADT adapted structs
since commit 69cc5370381e48352dc01c708599e9b49b7460f0 (hkaiser hkaiser
authored on 7 Nov 2009) spirit support BOOST_FUSION_ADAPT_ADT adapted
structs was broken

include/boost/spirit/home/qi/detail/assign_to.hpp
@@ -18,6 +18,7 @@
 #include <boost/spirit/home/qi/detail/attributes.hpp>
 #include <boost/spirit/home/support/container.hpp>
 #include <boost/fusion/include/copy.hpp>
+#include <boost/fusion/adapted/struct/detail/extension.hpp>
 #include <boost/ref.hpp>
 #include <boost/range/iterator_range.hpp>
 
@@ -200,6 +201,16 @@ namespace boost { namespace spirit { namespace traits
         call(boost::optional<T> const& val, Attribute& attr)
         {
             assign_to(val.get(), attr);
+        }
+    };
+
+    template <typename Attribute, int N, bool Const, typename T>
+    struct assign_to_attribute_from_value<fusion::extension::adt_attribute_proxy<Attribute, N, Const>, T>
+    {
+        static void
+        call(typename T const& val, typename fusion::extension::adt_attribute_proxy<Attribute, N, Const>& attr)
+        {
+            attr = val;
         }
     };
 

commit 90027e6a8e59f62189f72a4fefef9d82c18e14ce

Fix: remove extraneous 'typename'
Defect introduced by new code in a4f9ea9d58303b0d6a4347bcea4f30a9fdddebd2

include/boost/spirit/home/qi/detail/assign_to.hpp
@@ -208,7 +208,7 @@ namespace boost { namespace spirit { namespace traits
     struct assign_to_attribute_from_value<fusion::extension::adt_attribute_proxy<Attribute, N, Const>, T>
     {
         static void
-        call(typename T const& val, typename fusion::extension::adt_attribute_proxy<Attribute, N, Const>& attr)
+        call(T const& val, typename fusion::extension::adt_attribute_proxy<Attribute, N, Const>& attr)
         {
             attr = val;
         }