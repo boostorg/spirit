aed96ba915a276ed38115fdc19eb82f47343b907
mxc-commons <frank.hein@maxence.de>
2016-07-07 00:49:18 +0200

    spirit 2 expect directive
    Initial commit.

---
+++ include/boost/spirit/include/qi_expectd.hpp
@@ -0,0 +1,19 @@
+/*=============================================================================
+    Copyright (c) 2001-2011 Joel de Guzman
+    Copyright (c) 2001-2011 Hartmut Kaiser
+    Copyright (c) 2016      Frank Hein, maxence business consulting gmbh
+    http://spirit.sourceforge.net/
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
+#ifndef BOOST_SPIRIT_INCLUDE_QI_EXPECTD
+#define BOOST_SPIRIT_INCLUDE_QI_EXPECTD
+
+#if defined(_MSC_VER)
+#pragma once
+#endif
+
+#include <boost/spirit/home/qi/directive/expect.hpp>
+
+#endif
