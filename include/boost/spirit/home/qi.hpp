aa01de88d5b08cdc69d6fcb497842b705abe447c
mxc-commons <frank.hein@maxence.de>
2016-07-09 03:19:58 +0200

    Header reordering undo.

--- include/boost/spirit/home/qi.hpp
+++ include/boost/spirit/home/qi.hpp
@@ -17,10 +17,10 @@
 #include <boost/spirit/home/qi/char.hpp>
 #include <boost/spirit/home/qi/copy.hpp>
 #include <boost/spirit/home/qi/binary.hpp>
-#include <boost/spirit/home/qi/operator.hpp>
 #include <boost/spirit/home/qi/directive.hpp>
 #include <boost/spirit/home/qi/nonterminal.hpp>
 #include <boost/spirit/home/qi/numeric.hpp>
+#include <boost/spirit/home/qi/operator.hpp>
 #include <boost/spirit/home/qi/parse.hpp>
 #include <boost/spirit/home/qi/parse_attr.hpp>
 #include <boost/spirit/home/qi/parser.hpp>
