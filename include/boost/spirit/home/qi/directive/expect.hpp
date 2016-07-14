e399298e7b0c2c4989beabbc9ba337ef5fdaa94b
mxc-commons <frank.hein@maxence.de>
2016-07-15 01:11:36 +0200

    Adjustments for naming consistency
    The qi_expectd.hpp was removed. qi_expect.hpp now forwards to both
    directive and operator. Directive doc adjusted to reflect that change.
    
    Lexer.qbk restored to qi develop because change is not part of expect
    directive.

--- include/boost/spirit/home/qi/directive/expect.hpp
+++ include/boost/spirit/home/qi/directive/expect.hpp
@@ -4,8 +4,8 @@ Copyright (c) 2016 Frank Hein, maxence business consulting gmbh
 Distributed under the Boost Software License, Version 1.0. (See accompanying
 file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
-#if !defined (SPIRIT_EXPECTD_JULY_2016)
-#define SPIRIT_EXPECTD_JULY_2016
+#if !defined (SPIRIT_EXPECT_JULY_13_2016)
+#define SPIRIT_EXPECT_JULY_13_2016
 
 #if defined(_MSC_VER)
 #pragma once
