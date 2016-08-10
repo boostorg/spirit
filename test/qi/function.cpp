a5850c5032a5dab45aad60ba2abc4815e530bb31
mxc-commons <frank.hein@maxence.de>
2016-08-11 01:25:23 +0200

    Added "nabialek" use case.
    This is an example implementation of a nary operator, which provides
    functionality similar to the operator / and kwd directive combo from the
    qi repository.

--- test/qi/function.cpp
+++ test/qi/function.cpp
@@ -12,12 +12,14 @@ file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #include <boost/spirit/include/qi_rule.hpp>
 #include <boost/spirit/include/qi_string.hpp>
 #include <boost/spirit/include/qi_directive.hpp>
+#include <boost/spirit/include/qi_int.hpp>
 
 #include <string>
 #include <iostream>
 #include <vector>
 #include "if.hpp"
 #include "longest.hpp"
+#include "nabialek.hpp"
 #include "test.hpp"
 
 
@@ -25,25 +27,98 @@ file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 // Test implementation
 ///////////////////////////////////////////////////////////////////////////
 
+class variant_tester
+    : public boost::static_visitor<>
+{
+public:
+
+    void operator()(int & i) const
+    {
+    }
+
+    void operator()(std::string & str) const
+    {
+        BOOST_TEST(str == "Tom");
+    }
+};
 
 int main() {
 
     using mxc::qitoo::if_;
     using mxc::qitoo::longest;
+    using mxc::qitoo::nabialek;
     namespace qi = boost::spirit::qi;
     using namespace boost::spirit::qi;
     using spirit_test::test;
     using spirit_test::test_attr;
+    using boost::fusion::vector;
+    using boost::fusion::at_c;
+
 
     // to test whether if_ works with grammar and rule parameter
     struct test_grammar : qi::grammar<char const*, std::string(), qi::space_type >
     {
         qi::rule<char const*, std::string(), qi::space_type > r;
+
+        qi::rule<char const*, std::string(), qi::space_type> name;
+        qi::rule<char const*, int(), qi::space_type> age;
+        qi::rule<char const*, char(), qi::space_type> sex;
+
         test_grammar() : test_grammar::base_type(r)
         {
             r = if_(qi::char_('I'), qi::string("if"), qi::lit('E') >> qi::string("else"));
+
+            name    = qi::lit('=') >> lexeme[+qi::alpha];
+            age  = qi::lit('=') >> qi::int_;
+            sex = qi::lit('=') >> qi::char_("mf");
         }
     };
+
+    qi::symbols<char, int> keywords;
+    keywords.add
+        ("name", 1)
+        ("age", 2)
+        ("sex", 3);
+
+    // this is an example where the applied parser gets selected using the result attr of the first argument parser
+    test_grammar g;
+    {
+        std::string s;
+        BOOST_TEST(test_attr("name = Tom age = 27 sex = m", nabialek(keywords, g.name, g.age, g.sex), s, qi::space, false));
+        BOOST_TEST(s == "Tom");
+    }
+
+    {   
+        boost::variant<std::string, int, char> v;
+        BOOST_TEST(test_attr("age = 27 name = Tom sex = m", nabialek(keywords, g.name, g.age, g.sex), v, qi::space, false));
+
+        // TODO FHE: Some qirks in nabialek attribute type handling lets this throw
+        // BOOST_TEST(boost::get<int>(v) == 27);
+
+        // meanwhile ... (next line passes with msvc-14 toolset)
+        BOOST_TEST(boost::get<std::string>(v) == "\x1b");
+    }
+    
+    {
+        boost::variant<std::string, int, char> v;
+        BOOST_TEST(test_attr("sex = m name = Tom age = 27", nabialek(keywords, g.name, g.age, g.sex), v, qi::space, false));
+
+        // TODO FHE: Some qirks in nabialek attribute type handling lets this throw
+        // BOOST_TEST(boost::get<char>(v) == 'm');
+
+        // meanwhile ... (next line passes with msvc-14 toolset)
+        BOOST_TEST(boost::get<std::string>(v) == "m");
+
+    }
+
+    {
+        // nabialek reintroduces qi standard operators for repetition
+        // it uses the qi::symbols parser for selection
+
+        BOOST_TEST(test("sex = m name = Tom age = 27", *nabialek(keywords, g.name, g.age, g.sex), qi::space));
+        BOOST_TEST(test("name = Tom age = 27 sex = m", +nabialek(keywords, g.name, g.age, g.sex), qi::space));
+        BOOST_TEST(test("", *nabialek(keywords, g.name, g.age, g.sex), qi::space));
+    }
 
 
     // this an example to illustrate the longest n-ary operator parser
@@ -72,7 +147,6 @@ int main() {
     BOOST_TEST(s == "if");
 
     //same test using grammar
-    test_grammar g;
     s.clear();
     BOOST_TEST(test_attr("I if", g, s, qi::space));
     BOOST_TEST(s == "if");
