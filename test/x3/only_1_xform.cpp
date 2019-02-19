ab62ee432aab86d596a09ef59c4f2b7ddb79877b
Larry Evans <cppljevans@suddenlink.net>
2019-02-19 10:45:45 -0600

    Add the required copyright.

--- test/x3/only_1_xform.cpp
+++ test/x3/only_1_xform.cpp
@@ -1,3 +1,9 @@
+/*=============================================================================
+    Copyright (c) 2019 Larry Evans
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
 //Purpose:
 //  test that only 1 attribute transformation occurs.
 //Method:
