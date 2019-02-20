5ea06d04bdc2b33079d51b976541bf26c01107b0
Nikita Kniazev <nok.raven@gmail.com>
2019-02-20 21:51:01 +0300

    Refactor extract_int test

--- test/qi/extract_int.cpp
+++ test/qi/extract_int.cpp
@@ -69,63 +69,72 @@ public:
 
 }
 
+namespace qi = boost::spirit::qi;
+
+template <typename T, int Base, int MaxDigits>
+void test_overflow_handling(char const* begin, char const* end, int i)
+{
+    // Check that parser fails on overflow
+    BOOST_STATIC_ASSERT_MSG(std::numeric_limits<T>::is_bounded, "tests prerequest");
+    BOOST_ASSERT_MSG(MaxDigits == -1 || std::pow(Base, MaxDigits) >= T::max,
+                     "test prerequest");
+    int initial = Base - i % Base; // just a 'random' non-equal to i number
+    T x(initial);
+    char const* it = begin;
+    bool r = qi::extract_int<T, Base, 1, MaxDigits>::call(it, end, x);
+    if (T::min <= i && i <= T::max) {
+        BOOST_TEST(r);
+        BOOST_TEST(it == end);
+        BOOST_TEST_EQ(x, i);
+    }
+    else {
+        BOOST_TEST(!r);
+        BOOST_TEST(it == begin);
+    }
+}
+
+template <typename T, int Base>
+void test_unparsed_digits_are_not_consumed(char const* it, char const* end, int i)
+{
+    // Check that unparsed digits are not consumed
+    BOOST_STATIC_ASSERT_MSG(T::min <= -Base, "test prerequest");
+    BOOST_STATIC_ASSERT_MSG(T::max >=  Base, "test prerequest");
+    bool has_sign = *it == '+' || *it == '-';
+    char const* begin = it;
+    int initial = Base - i % Base; // just a 'random' non-equal to i number
+    T x(initial);
+    bool r = qi::extract_int<T, Base, 1, 1>::call(it, end, x);
+    BOOST_TEST(r);
+    if (-Base < i && i < Base) {
+        BOOST_TEST(it == end);
+        BOOST_TEST_EQ(x, i);
+    }
+    else {
+        BOOST_TEST_EQ(end - it, (end - begin) - 1 - has_sign);
+        BOOST_TEST_EQ(x, i / Base);
+    }
+}
+
+template <typename T, int Base>
+void run_tests(char const* begin, char const* end, int i)
+{
+    // Check that parser fails on overflow
+    test_overflow_handling<T, Base, -1>(begin, end, i);
+    // Check that MaxDigits > digits10 behave like MaxDigits=-1
+    test_overflow_handling<T, Base, 2>(begin, end, i);
+    // Check that unparsed digits are not consumed
+    test_unparsed_digits_are_not_consumed<T, Base>(begin, end, i);
+}
+
 int main()
 {
-    namespace qi = boost::spirit::qi;
-
     for (int i = -20; i <= 20; ++i) {
-        int initial = i > 0 ? -9 : 9;
-        custom_int x = initial;
         std::ostringstream oss;
         oss << i;
         std::string s = oss.str();
-        char const* begin = s.data(), * it = begin, *const end = it + s.size();
+        char const* begin = s.data(), *const end = begin + s.size();
 
-        // Test prerequest: Limits::is_bounded == true
-        // Check that parser fails on overflow
-        bool r = qi::extract_int<custom_int, 10, 1, -1>::call(it, end, x);
-        if (custom_int::min <= i && i <= custom_int::max) {
-            BOOST_TEST(r);
-            BOOST_TEST(it == end);
-            BOOST_TEST_EQ(x, i);
-        }
-        else {
-            BOOST_TEST(!r);
-            BOOST_TEST(it == begin);
-        }
-
-        // Test prerequest: MaxDigits == log10(Limits::digits)
-        //                  MaxDigits > Limits::digits10
-        // Check that MaxDigits > digits10 behave like MaxDigits=-1
-        it = begin;
-        x = initial;
-        r = qi::extract_int<custom_int, 10, 1, 2>::call(it, end, x);
-        if (custom_int::min <= i && i <= custom_int::max) {
-            BOOST_TEST(r);
-            BOOST_TEST(it == end);
-            BOOST_TEST_EQ(x, i);
-        }
-        else {
-            BOOST_TEST(!r);
-            BOOST_TEST(it == begin);
-        }
-
-        // Test prerequest: MaxDigits < Limits::digits10
-        // Check that unparsed digits are not consumed
-        it = begin;
-        x = initial;
-        r = qi::extract_int<custom_int, 10, 1, 1>::call(it, end, x);
-        BOOST_STATIC_ASSERT(custom_int::min < -9 && custom_int::max > 9);
-        BOOST_TEST(r);
-        if (-9 <= i && i <= 9) {
-            BOOST_TEST(it == end);
-            BOOST_TEST_EQ(x, i);
-        }
-        else {
-            BOOST_ASSERT(-100 < i && i < 100);
-            BOOST_TEST_EQ(end - it, 1);
-            BOOST_TEST_EQ(x, i / 10);
-        }
+        run_tests<custom_int, 10>(begin, end, i);
     }
 
     return boost::report_errors();
