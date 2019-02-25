8bd9b6ece40f9324a902bb0f726e785ffa689fce
Nikita Kniazev <nok.raven@gmail.com>
2019-02-26 02:23:36 +0300

    X3: Fixed container of move only types with sequence

--- include/boost/spirit/home/x3/core/detail/parse_into_container.hpp
+++ include/boost/spirit/home/x3/core/detail/parse_into_container.hpp
@@ -21,6 +21,7 @@
 #include <boost/fusion/include/front.hpp>
 #include <boost/fusion/include/back.hpp>
 #include <boost/variant/apply_visitor.hpp>
+#include <iterator> // for std::make_move_iterator
 
 namespace boost { namespace spirit { namespace x3 { namespace detail
 {
@@ -262,7 +263,8 @@ namespace boost { namespace spirit { namespace x3 { namespace detail
             Attribute rest;
             bool r = parser.parse(first, last, context, rcontext, rest);
             if (r)
-                traits::append(attr, rest.begin(), rest.end());
+                traits::append(attr, std::make_move_iterator(rest.begin()),
+                                     std::make_move_iterator(rest.end()));
             return r;
         }
 
