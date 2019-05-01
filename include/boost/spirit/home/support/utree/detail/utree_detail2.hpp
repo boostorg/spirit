5045fe6b5367216368ec007e72ed6c208e631a3f
Nikita Kniazev <nok.raven@gmail.com>
2019-05-01 20:22:54 +0300

    utree: Missing cstring include. Fixes #502

--- include/boost/spirit/home/support/utree/detail/utree_detail2.hpp
+++ include/boost/spirit/home/support/utree/detail/utree_detail2.hpp
@@ -19,6 +19,7 @@
 #include <boost/utility/enable_if.hpp>
 #include <boost/throw_exception.hpp>
 #include <boost/iterator/iterator_traits.hpp>
+#include <cstring> // for std::memcpy
 
 namespace boost { namespace spirit { namespace detail
 {
