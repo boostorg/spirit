boostorg/spirit

commit d4c0179fa6c659ce4759c4438c3623c410d790d8

X3: parse_rule attribute must always be of rule::attribute_type&
After #456 `parse_rule` is always called with value of `rule::attribute_type&`
type. By removing attribute type deduction we also will turn any possible bug
in `transform_attribute` from a linkage to compile error.

Fixes #454

include/boost/spirit/home/x3/nonterminal/rule.hpp
@@ -166,11 +166,11 @@ namespace boost { namespace spirit { namespace x3
     };
 
 #define BOOST_SPIRIT_DECLARE_(r, data, rule_type)                               \
-    template <typename Iterator, typename Context, typename Attribute>          \
+    template <typename Iterator, typename Context>                              \
     bool parse_rule(                                                            \
         rule_type rule_                                                         \
       , Iterator& first, Iterator const& last                                   \
-      , Context const& context, Attribute& attr);                               \
+      , Context const& context, rule_type::attribute_type& attr);               \
     /***/
 
 #define BOOST_SPIRIT_DECLARE(...) BOOST_PP_SEQ_FOR_EACH(                        \
@@ -180,11 +180,11 @@ namespace boost { namespace spirit { namespace x3
 #if BOOST_WORKAROUND(BOOST_MSVC, < 1910)
 #define BOOST_SPIRIT_DEFINE_(r, data, rule_name)                                \
     using BOOST_PP_CAT(rule_name, _synonym) = decltype(rule_name);              \
-    template <typename Iterator, typename Context, typename Attribute>          \
+    template <typename Iterator, typename Context>                              \
     inline bool parse_rule(                                                     \
         BOOST_PP_CAT(rule_name, _synonym) /* rule_ */                           \
       , Iterator& first, Iterator const& last                                   \
-      , Context const& context, Attribute& attr)                                \
+      , Context const& context, BOOST_PP_CAT(rule_name, _synonym)::attribute_type& attr) \
     {                                                                           \
         using boost::spirit::x3::unused;                                        \
         static auto const def_ = (rule_name = BOOST_PP_CAT(rule_name, _def));   \
@@ -193,11 +193,11 @@ namespace boost { namespace spirit { namespace x3
     /***/
 #else
 #define BOOST_SPIRIT_DEFINE_(r, data, rule_name)                                \
-    template <typename Iterator, typename Context, typename Attribute>          \
+    template <typename Iterator, typename Context>                              \
     inline bool parse_rule(                                                     \
         decltype(rule_name) /* rule_ */                                         \
       , Iterator& first, Iterator const& last                                   \
-      , Context const& context, Attribute& attr)                                \
+      , Context const& context, decltype(rule_name)::attribute_type& attr)      \
     {                                                                           \
         using boost::spirit::x3::unused;                                        \
         static auto const def_ = (rule_name = BOOST_PP_CAT(rule_name, _def));   \
@@ -211,7 +211,7 @@ namespace boost { namespace spirit { namespace x3
     /***/
 
 #define BOOST_SPIRIT_INSTANTIATE(rule_type, Iterator, Context)                  \
-    template bool parse_rule<Iterator, Context, rule_type::attribute_type>(     \
+    template bool parse_rule<Iterator, Context>(                                \
         rule_type rule_                                                         \
       , Iterator& first, Iterator const& last                                   \
       , Context const& context, rule_type::attribute_type&);                    \

doc/x3/tutorial/minimal.qbk
@@ -90,11 +90,11 @@ happening is that we are declaring a `parse_rule` function in the client
 namespace. For example, given a rule named `my_rule`,
 `BOOST_SPIRIT_DECLARE(my_rule)` expands to this code:
 
-    template <typename Iterator, typename Context, typename Attribute>
+    template <typename Iterator, typename Context>
     bool parse_rule(
         decltype(my_rule)
       , Iterator& first, Iterator const& last
-      , Context const& context, Attribute& attr);
+      , Context const& context, decltype(my_rule)::attribute_type& attr);
 
 If you went back and reviewed [link __tutorial_spirit_define__
 BOOST_SPIRIT_DEFINE], you'll see why it is exactly what we need to use for

doc/x3/tutorial/roman.qbk
@@ -172,11 +172,11 @@ function in the client namespace that tells X3 how to invoke the rule. For examp
 given a rule named `my_rule` and a corresponding definition named `my_rule_def`,
 `BOOST_SPIRIT_DEFINE(my_rule)` expands to this code:
 
-    template <typename Iterator, typename Context, typename Attribute>
+    template <typename Iterator, typename Context>
     inline bool parse_rule(
         decltype(my_rule)
       , Iterator& first, Iterator const& last
-      , Context const& context, Attribute& attr)
+      , Context const& context, decltype(my_rule)::attribute_type& attr)
     {
         using boost::spirit::x3::unused;
         static auto const def_ = my_rule_def;

commit 10d027fd5cfd573471e80e2e3c2d0cb0616a6d65

X3: Fix `parse_rule` definition check
Regressed in #457

include/boost/spirit/home/x3/nonterminal/detail/rule.hpp
@@ -20,6 +20,8 @@
 #include <boost/spirit/home/x3/nonterminal/simple_trace.hpp>
 #endif
 
+#include <type_traits>
+
 namespace boost { namespace spirit { namespace x3
 {
     template <typename ID>
@@ -191,7 +193,7 @@ namespace boost { namespace spirit { namespace x3 { namespace detail
             typedef
                 decltype(parse_rule(
                     rule<ID, Attribute>(), first, last
-                  , make_unique_context<ID>(rhs, context), attr))
+                  , make_unique_context<ID>(rhs, context), std::declval<Attribute&>()))
             parse_rule_result;
 
             // If there is no BOOST_SPIRIT_DEFINE for this rule,

test/x3/rule_separate_tu.cpp
@@ -12,6 +12,30 @@
 
 #include "test.hpp"
 
+namespace sem_act {
+
+namespace x3 = boost::spirit::x3;
+
+auto nop = [](auto const&){};
+
+x3::rule<class used_attr1_r, int> used_attr1;
+auto const used_attr1_def = used_attr::grammar[nop];
+BOOST_SPIRIT_DEFINE(used_attr1);
+
+x3::rule<class used_attr2_r, int> used_attr2;
+auto const used_attr2_def = unused_attr::grammar[nop];
+BOOST_SPIRIT_DEFINE(used_attr2);
+
+x3::rule<class unused_attr1_r> unused_attr1;
+auto const unused_attr1_def = used_attr::grammar[nop];
+BOOST_SPIRIT_DEFINE(unused_attr1);
+
+x3::rule<class unused_attr2_r> unused_attr2;
+auto const unused_attr2_def = unused_attr::grammar[nop];
+BOOST_SPIRIT_DEFINE(unused_attr2);
+
+}
+
 int main()
 {
     using spirit_test::test;
@@ -33,6 +57,14 @@ int main()
         BOOST_TEST_EQ(i, 123);
         BOOST_TEST(test_attr(" 42", used_attr::grammar, i, used_attr::skipper));
         BOOST_TEST_EQ(i, 42);
+    }
+
+    {
+        long i;
+        BOOST_TEST(test_attr("123", sem_act::used_attr1, i));
+        BOOST_TEST(test_attr("===", sem_act::used_attr2, i));
+        BOOST_TEST(test("123", sem_act::unused_attr1));
+        BOOST_TEST(test("===", sem_act::unused_attr2));
     }
 
     return boost::report_errors();