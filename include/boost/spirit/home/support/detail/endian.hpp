4b58ddbeafb6549339c1044d2807deca4bedfebd
Nikita Kniazev <nok.raven@gmail.com>
2019-11-04 16:04:56 +0300

    V2: Use Boost.Endian
    And drop in-house implementation.

--- include/boost/spirit/home/support/detail/endian.hpp
+++ include/boost/spirit/home/support/detail/endian.hpp
@@ -11,19 +11,11 @@
 #pragma once
 #endif
 
-#include <boost/version.hpp>
-
 // We need to treat the endian number types as PODs
 #if !defined(BOOST_ENDIAN_FORCE_PODNESS)
 #define BOOST_ENDIAN_FORCE_PODNESS 1
 #endif
 
-// If Boost has the endian library, use it, otherwise use an adapted version
-// included with Spirit
-// #if BOOST_VERSION >= 105100
-// #include <boost/endian/integers.hpp>
-// #else
-#include <boost/spirit/home/support/detail/endian/endian.hpp>
-// #endif
+#include <boost/endian/arithmetic.hpp>
 
 #endif
