2a231e81a3a6e4b540d7f31e287cfc1be095db52
Nikita Kniazev <nok.raven@gmail.com>
2021-12-17 04:39:58 +0300

    Lex: Fix warning C4458: declaration of 'varname' hides class member

--- include/boost/spirit/home/lex/lexer/lexer.hpp
+++ include/boost/spirit/home/lex/lexer/lexer.hpp
@@ -235,19 +235,19 @@ namespace boost { namespace spirit { namespace lex
             {}
 
             // allow to switch states
-            lexer_def_ operator()(char_type const* state) const
+            lexer_def_ operator()(char_type const* state_) const
             {
-                return lexer_def_(def, state);
+                return lexer_def_(def, state_);
             }
-            lexer_def_ operator()(char_type const* state
-              , char_type const* targetstate) const
+            lexer_def_ operator()(char_type const* state_
+              , char_type const* targetstate_) const
             {
-                return lexer_def_(def, state, targetstate);
+                return lexer_def_(def, state_, targetstate_);
             }
-            lexer_def_ operator()(string_type const& state
-              , string_type const& targetstate = string_type()) const
+            lexer_def_ operator()(string_type const& state_
+              , string_type const& targetstate_ = string_type()) const
             {
-                return lexer_def_(def, state, targetstate);
+                return lexer_def_(def, state_, targetstate_);
             }
 
             // allow to assign a token definition expression
@@ -267,9 +267,9 @@ namespace boost { namespace spirit { namespace lex
 
             // explicitly tell the lexer that the given state will be defined
             // (useful in conjunction with "*")
-            std::size_t add_state(char_type const* state = 0)
+            std::size_t add_state(char_type const* state_ = 0)
             {
-                return def.add_state(state ? state : def.initial_state().c_str());
+                return def.add_state(state_ ? state_ : def.initial_state().c_str());
             }
 
             adder add;
