6a900283196ff015ef34c482f2e3ec1a98b1f25c
Nikita Kniazev <nok.raven@gmail.com>
2022-01-06 02:23:31 +0300

    X3: Warn about future C++17 requirement
    I plan to keep the warning for two releases before starting to utilize C++17 features.
    
    Since Boost 1.81 (scheduled to November 2022) supported compilers will be:
    * Clang 4 (currently 3.6)
    * GCC 7 (currently 5)
    * VS 2017 (currently 2015)

--- include/boost/spirit/home/x3.hpp
+++ include/boost/spirit/home/x3.hpp
@@ -11,6 +11,40 @@
 #pragma once
 #endif
 
+#if !defined BOOST_SPIRIT_X3_HIDE_CXX17_WARNING && !( true \
+  && defined __cpp_if_constexpr && __cpp_if_constexpr >= 201606 \
+  && defined __cpp_inline_variables && __cpp_inline_variables >= 201606 \
+  && defined __cpp_fold_expressions && __cpp_fold_expressions >= 201603 \
+  && defined __cpp_variadic_using && __cpp_variadic_using >= 201611 \
+  && (defined __cpp_template_auto && __cpp_template_auto >= 201606 \
+      || defined __cpp_nontype_template_parameter_auto && __cpp_nontype_template_parameter_auto >= 201606) \
+  && defined __cpp_nontype_template_args && __cpp_nontype_template_args >= 201411 \
+)
+# define BOOST_SPIRIT_X3_STRINGIZE_IMPL(x) #x
+# define BOOST_SPIRIT_X3_STRINGIZE(x) BOOST_SPIRIT_X3_STRINGIZE_IMPL(x)
+# if defined __GNUC__ || defined __clang__
+#  warning "Spirit X3 will soon use C++17 features which your compiler does not support"
+#  warning "Minimal supported compiler versions: Clang 4 / GCC 7 / MSC 1915 (VS 2017 v15.8)"
+#  if defined __clang__
+#   pragma message "This compiler seems to be Clang " BOOST_SPIRIT_X3_STRINGIZE(__clang_major__)
+#  else
+#   pragma message "This compiler seems to be GCC " BOOST_SPIRIT_X3_STRINGIZE(__GNUC__)
+#  endif
+#  warning "Define BOOST_SPIRIT_X3_HIDE_CXX17_WARNING to hide the warning"
+# elif defined _MSC_VER
+#  pragma message (__FILE__ "(" BOOST_SPIRIT_X3_STRINGIZE(__LINE__) "): warning: Spirit X3 will soon use C++17 features which your compiler does not support" )
+#  pragma message (__FILE__ "(" BOOST_SPIRIT_X3_STRINGIZE(__LINE__) "): warning: Minimal supported compiler versions: Clang 4 / GCC 7 / MSC 1915 (VS 2017 v15.8)" )
+#  pragma message (__FILE__ "(" BOOST_SPIRIT_X3_STRINGIZE(__LINE__) "): warning: This compiler seems to be MSC " BOOST_SPIRIT_X3_STRINGIZE(_MSC_VER) )
+#  pragma message (__FILE__ "(" BOOST_SPIRIT_X3_STRINGIZE(__LINE__) "): warning: Define BOOST_SPIRIT_X3_HIDE_CXX17_WARNING to hide the warning" )
+# else
+#  pragma message "warning: Spirit X3 will soon use C++17 features which your compiler does not support"
+#  pragma message "warning: Minimal supported compiler versions: Clang 4 / GCC 7 / MSC 1915 (VS 2017 v15.8)"
+#  pragma message "warning: Define BOOST_SPIRIT_X3_HIDE_CXX17_WARNING to hide the warning"
+# endif
+# undef BOOST_SPIRIT_X3_STRINGIZE
+# undef BOOST_SPIRIT_X3_STRINGIZE_IMPL
+#endif
+
 #include <boost/spirit/home/x3/auxiliary.hpp>
 #include <boost/spirit/home/x3/char.hpp>
 #include <boost/spirit/home/x3/directive.hpp>
