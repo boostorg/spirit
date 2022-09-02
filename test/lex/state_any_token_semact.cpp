32fb6f689957c121973594e2505fd959a917291e
Nikita Kniazev <nok.raven@gmail.com>
2022-09-03 02:07:20 +0300

    V2.Tests: Revise poorly named tests
    Looking by the commit history they should not have been using classic iterators

--- test/lex/state_any_token_semact.cpp
+++ test/lex/state_any_token_semact.cpp
@@ -1,52 +1,39 @@
-//  Copyright (c) 2001-2010 Hartmut Kaiser
+//  Copyright (c) 2001-2011 Hartmut Kaiser
 //  Copyright (c) 2010 Mathias Gaunard
 // 
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <boost/spirit/include/support_multi_pass.hpp>
-#include <boost/spirit/include/classic_position_iterator.hpp>
+// lexers with semantic actions attached to state '*' tokens
+
 #include <boost/spirit/include/lex_lexertl.hpp>
 
 #include <boost/core/lightweight_test.hpp>
 #include <boost/phoenix/operator/self.hpp>
-#include <boost/phoenix/statement/sequence.hpp>
 
 #include <sstream>
 
 namespace spirit = boost::spirit;
 namespace lex = spirit::lex;
 
-typedef spirit::classic::position_iterator2<
-    spirit::multi_pass<std::istreambuf_iterator<char> >
-> file_iterator;
-
-inline file_iterator 
-make_file_iterator(std::istream& input, const std::string& filename)
-{
-    return file_iterator(
-        spirit::make_default_multi_pass(
-            std::istreambuf_iterator<char>(input)),
-        spirit::multi_pass<std::istreambuf_iterator<char> >(),
-        filename);
-}
+typedef char const* content_iterator;
 
 struct identifier
 {
-    identifier(file_iterator, file_iterator)
+    identifier(content_iterator, content_iterator)
     {
     }
 };
 
 struct string_literal
 {
-    string_literal(file_iterator, file_iterator)
+    string_literal(content_iterator, content_iterator)
     {
     }
 };
 
 typedef lex::lexertl::token<
-    file_iterator, boost::mpl::vector<identifier, string_literal>
+    content_iterator, boost::mpl::vector<identifier, string_literal>
 > token_type;
 
 struct lexer
@@ -56,22 +43,13 @@ struct lexer
       : id("[a-zA-Z0-9]+", 1)
       , st("'[^'\\n]*'", 2)
     {
-        self =  id [ 
-                    lex::_state = "ST"
-                ]
-            |   lex::token_def<>(".", 3) [ 
-                    lex::_state = "ST" 
-                ]
+        self("ST") = 
+                st [ lex::_state = "INITIAL" ] 
             ;
         
-        self("ST") =
-                st [ 
-                    lex::_state = "INITIAL" 
-                ]
-            |   lex::token_def<>(".", 4) [(
-                    lex::_state = "INITIAL"
-                  , lex::_pass = lex::pass_flags::pass_fail 
-                )]
+        self("*") =  
+                id                       [ lex::_state = "ST" ]
+            |   lex::token_def<>(".", 3) [ lex::_state = "ST" ]
             ;
     }
     
@@ -83,11 +61,10 @@ typedef lexer::iterator_type token_iterator;
 
 int main()
 {
-    std::stringstream ss;
-    ss << "foo 'bar'";
+    std::string const s = "foo 'bar'";
     
-    file_iterator begin = make_file_iterator(ss, "SS");
-    file_iterator end;
+    content_iterator begin = s.data();
+    content_iterator end = s.data() + s.size();
     
     lexer l;
     token_iterator begin2 = l.begin(begin, end, "ST");
