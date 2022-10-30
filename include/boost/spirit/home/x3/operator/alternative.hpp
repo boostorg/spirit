73a865b5350e57192cb345aad5cf6fc5a72d147d
eido79 <eido79@gmail.com>
2022-10-30 22:32:51 +0100

    Rework attribute for alternative parser
    Implement the following rules
    * Remove duplicates
    * Simplify variant<X> to X
    * Make unused_type the first type of the variant
    * Transform variant<unused_type, T> to optional<T>

--- include/boost/spirit/home/x3/operator/alternative.hpp
+++ include/boost/spirit/home/x3/operator/alternative.hpp
@@ -7,11 +7,12 @@
 #if !defined(BOOST_SPIRIT_X3_ALTERNATIVE_JAN_07_2013_1131AM)
 #define BOOST_SPIRIT_X3_ALTERNATIVE_JAN_07_2013_1131AM
 
-#include <boost/spirit/home/x3/support/traits/attribute_of_binary.hpp>
 #include <boost/spirit/home/x3/core/parser.hpp>
 #include <boost/spirit/home/x3/operator/detail/alternative.hpp>
-
+#include <boost/spirit/home/x3/support/meta.hpp>
 #include <boost/variant/variant_fwd.hpp>
+
+#include <type_traits>
 
 namespace boost { namespace spirit { namespace x3
 {
@@ -53,11 +54,96 @@ namespace boost { namespace spirit { namespace x3
     }
 }}}
 
+
+namespace boost { namespace spirit { namespace x3 { namespace detail
+{
+    template <typename Seq, typename... Ts>
+    struct add_alternative_types_impl;
+
+    template <template<class...> typename Seq, typename... Ts>
+    struct add_alternative_types_impl<Seq<Ts...>>
+    {
+        using type = Seq<Ts...>;
+    };
+
+    template <template<class...> typename Seq, typename... Ts, typename U, typename... Us>
+    struct add_alternative_types_impl<Seq<Ts...>, U, Us...>
+    {
+        using next_sequence = conditional_t<Seq<Ts...>::template contains<U>,
+            Seq<Ts...>,
+            conditional_t<std::is_same_v<std::remove_const_t<U>, unused_type>,
+                typename Seq<Ts...>::template prepend<U>,
+                typename Seq<Ts...>::template append<U>
+            >
+        >;
+
+        using type = typename add_alternative_types_impl<next_sequence, Us...>::type;
+    };
+
+    template <typename Seq, typename... Ts>
+    using add_alternative_types = typename add_alternative_types_impl<Seq, Ts...>::type;
+
+    template <typename... Seqs>
+    struct merge_types_of_alternative_impl;
+
+    template <template <class...> typename Seq1, typename... T1s, template <class...> typename Seq2, typename... T2s>
+    struct merge_types_of_alternative_impl<Seq1<T1s...>, Seq2<T2s...>>
+    {
+        using type = add_alternative_types<Seq1<T1s...>, T2s...>;
+    };
+
+    template <typename... Seqs>
+    using merge_types_of_alternative = typename merge_types_of_alternative_impl<Seqs...>::type;
+
+    template <typename P, typename C>
+    struct get_types_of_alternative
+    {
+        using type = type_sequence<typename traits::attribute_of<P, C>::type>;
+    };
+
+    template <typename L, typename R, typename C>
+    struct get_types_of_alternative<alternative<L, R>, C>
+    {
+        using type = merge_types_of_alternative<
+            typename get_types_of_alternative<L, C>::type,
+            typename get_types_of_alternative<R, C>::type
+        >;
+    };
+
+    template <template <typename...> typename A, typename Seq>
+    struct type_sequence_to_alternative_attribute;
+
+    template <template <typename...> typename A, template <typename...> typename Seq>
+    struct type_sequence_to_alternative_attribute<A, Seq<>>
+    {
+        using type = unused_type;
+    };
+
+    template <template <typename...> typename A, template <typename...> typename Seq, typename T, typename... Ts>
+    struct type_sequence_to_alternative_attribute<A, Seq<T, Ts...>>
+    {
+        using type = conditional_t<sizeof...(Ts) == 0,
+            T,
+            A<T, Ts...>
+        >;
+    };
+
+    template <template <typename...> typename A, template <typename...> typename Seq, typename T>
+    struct type_sequence_to_alternative_attribute<A, Seq<unused_type, T>>
+    {
+        using type = boost::optional<T>;
+    };
+
+    template <template <typename...> class A, typename P, typename C>
+    using attribute_of_alternative = type_sequence_to_alternative_attribute<A,
+        typename get_types_of_alternative<P, C>::type>;
+}}}}
+
 namespace boost { namespace spirit { namespace x3 { namespace traits
 {
     template <typename Left, typename Right, typename Context>
     struct attribute_of<x3::alternative<Left, Right>, Context>
-        : x3::detail::attribute_of_binary<boost::variant, x3::alternative, Left, Right, Context> {};
+        : x3::detail::attribute_of_alternative<boost::variant, x3::alternative<Left, Right>, Context> {};
 }}}}
 
 #endif
--- include/boost/spirit/home/x3/operator/sequence.hpp
+++ include/boost/spirit/home/x3/operator/sequence.hpp
@@ -7,10 +7,11 @@
 #if !defined(BOOST_SPIRIT_X3_SEQUENCE_JAN_06_2013_1015AM)
 #define BOOST_SPIRIT_X3_SEQUENCE_JAN_06_2013_1015AM
 
-#include <boost/spirit/home/x3/support/traits/attribute_of_binary.hpp>
 #include <boost/spirit/home/x3/core/parser.hpp>
 #include <boost/spirit/home/x3/operator/detail/sequence.hpp>
 #include <boost/spirit/home/x3/directive/expect.hpp>
+#include <boost/spirit/home/x3/support/meta.hpp>
+#include <boost/spirit/home/x3/support/unused.hpp>
 
 #include <boost/fusion/include/deque_fwd.hpp>
 
@@ -64,11 +65,40 @@ namespace boost { namespace spirit { namespace x3
     }
 }}}
 
+namespace boost { namespace spirit { namespace x3 { namespace detail
+{
+    template <typename Attribute>
+    struct types_of_sequence_init : type_sequence<Attribute> {};
+    template <>
+    struct types_of_sequence_init<unused_type> : type_sequence<> {};
+    template <>
+    struct types_of_sequence_init<unused_type const> : type_sequence<> {};
+
+    template <typename P, typename C>
+    struct get_types_of_sequence
+      : types_of_sequence_init<typename traits::attribute_of<P, C>::type> {};
+
+    template <typename L, typename R, typename C>
+    struct get_types_of_sequence<x3::sequence<L, R>, C>
+      : get_types_of_sequence<L, C>::template extend<get_types_of_sequence<R, C>> {};
+
+    template <template <typename...> class A, typename T, int = T::size>
+    struct type_sequence_to_attribute { using type = typename T::template transfer_to<A>; };
+    template <template <typename...> class A, typename T>
+    struct type_sequence_to_attribute<A, T, 1> : T::template transfer_to<type_identity> {};
+    template <template <typename...> class A, typename T>
+    struct type_sequence_to_attribute<A, T, 0> { using type = unused_type; };
+
+    template <template <typename...> class A, typename P, typename C>
+    using attribute_of_sequence = type_sequence_to_attribute<A,
+                                    typename get_types_of_sequence<P, C>::type>;
+}}}}
+
 namespace boost { namespace spirit { namespace x3 { namespace traits
 {
     template <typename Left, typename Right, typename Context>
     struct attribute_of<x3::sequence<Left, Right>, Context>
-        : x3::detail::attribute_of_binary<fusion::deque, x3::sequence, Left, Right, Context> {};
+        : x3::detail::attribute_of_sequence<fusion::deque, x3::sequence<Left, Right>, Context> {};
 }}}}
 
 #endif
