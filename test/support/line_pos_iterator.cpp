b04e780a34c46a8f153c4398c0f083af8953ef10
Nikita Kniazev <nok.raven@gmail.com>
2023-03-07 22:58:08 +0300

    Tests: Refactor line_pos_iterator test

--- test/support/line_pos_iterator.cpp
+++ test/support/line_pos_iterator.cpp
@@ -1,4 +1,5 @@
 //  Copyright (c) 2014 Tomoki Imai
+//  Copyright (c) 2023 Nikita Knizev
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying 
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -12,23 +13,23 @@
 #include <vector>
 
 struct validation {
-    validation()
-    : line(), column(), current(), is_defined(false) {
+    validation(size_t line, size_t column)
+    : line(line), column(column), inside_newline(true) {
     }
 
     validation(size_t line, size_t column, std::string current)
-    : line(line), column(column), current(current), is_defined(true) {
+    : line(line), column(column), current(current), inside_newline(false) {
     }
 
     size_t line;
     size_t column;
     std::string current;
-    bool is_defined;
+    bool inside_newline;
 };
 
 typedef std::vector<validation> validations;
 
-void test(std::string const& input, validations const& validations) {
+void test(std::string const& input, validations const& validations, bool singlechar_newline) {
     typedef boost::spirit::line_pos_iterator<std::string::const_iterator> pos_iterator_t;
 
     pos_iterator_t const input_begin(input.begin());
@@ -37,9 +38,9 @@ void test(std::string const& input, validations const& validations) {
     validations::const_iterator expected = validations.begin();
 
     for (; position != input_end && expected != validations.end(); ++position, ++expected) {
-        if (!expected->is_defined)
-        	continue;
-
+        if (expected->inside_newline && singlechar_newline)
+            if (++expected == validations.end())
+                break;
         boost::iterator_range<pos_iterator_t> const range = get_current_line(input_begin, position, input_end);
         std::string const current(range.begin(), range.end());
 
@@ -47,104 +48,61 @@ void test(std::string const& input, validations const& validations) {
         BOOST_TEST_EQ(expected->column, get_column(input_begin, position));
         BOOST_TEST_EQ(expected->current, current);
     }
+    if (expected != validations.end() && expected->inside_newline && singlechar_newline)
+        ++expected;
 
     BOOST_TEST(position == input_end);
     BOOST_TEST(expected == validations.end());
 }
 
-// LR and CR
-
-void testLRandCR(std::string const& line_break) {
+void test_only(std::string const& line_break) {
     std::string const input = line_break + line_break;
     validations const validations = boost::assign::list_of<validation>
-        (1,1,"")()
-        (2,1,"")();
-    test(input, validations);
+        (1,1,"")(2,1)
+        (2,1,"")(2,1);
+    test(input, validations, line_break.size() == 1);
 }
 
-void testLRandCR_foo_bar_git(std::string const& line_break) {
+void test_in_text(std::string const& line_break) {
     std::string const input = "foo" + line_break + "bar" + line_break + "git";
     validations const validations = boost::assign::list_of<validation>
-        (1,1,"foo")(1,2,"foo")(1,3,"foo")(1,4,"foo")()
-        (2,1,"bar")(2,2,"bar")(2,3,"bar")(2,4,"bar")()
+        (1,1,"foo")(1,2,"foo")(1,3,"foo")(1,4,"foo")(2,1)
+        (2,1,"bar")(2,2,"bar")(2,3,"bar")(2,4,"bar")(3,1)
         (3,1,"git")(3,2,"git")(3,3,"git");
-    test(input, validations);
+    test(input, validations, line_break.size() == 1);
 }
 
-void testLRandCR_bar_git(std::string const& line_break) {
+void test_at_begin(std::string const& line_break) {
     std::string const input = line_break + "bar" + line_break + "git";
     validations const validations = boost::assign::list_of<validation>
-        (1,1,"")()
-        (2,1,"bar")(2,2,"bar")(2,3,"bar")(2,4,"bar")()
+        (1,1,"")(2,1)
+        (2,1,"bar")(2,2,"bar")(2,3,"bar")(2,4,"bar")(3,1)
         (3,1,"git")(3,2,"git")(3,3,"git");
-    test(input, validations);
+    test(input, validations, line_break.size() == 1);
 }
 
-void testLRandCR_foo_bar(std::string const& line_break) {
+void test_at_end(std::string const& line_break) {
     std::string const input = "foo" + line_break + "bar" + line_break;
     validations const validations = boost::assign::list_of<validation>
-        (1,1,"foo")(1,2,"foo")(1,3,"foo")(1,4,"foo")()
-        (2,1,"bar")(2,2,"bar")(2,3,"bar")(2,4,"bar")();
-    test(input, validations);
+        (1,1,"foo")(1,2,"foo")(1,3,"foo")(1,4,"foo")(2,1)
+        (2,1,"bar")(2,2,"bar")(2,3,"bar")(2,4,"bar")(3,1);
+    test(input, validations, line_break.size() == 1);
 }
 
-// LR or CR
-
-void testLRorCR(std::string const& line_break) {
-    std::string const input = line_break + line_break;
-    validations const validations = boost::assign::list_of<validation>
-        (1,1,"")
-        (2,1,"");
-    test(input, validations);
-}
-
-void testLRorCR_foo_bar_git(std::string const& line_break) {
-    std::string const input = "foo" + line_break + "bar" + line_break + "git";
-    validations const validations = boost::assign::list_of<validation>
-        (1,1,"foo")(1,2,"foo")(1,3,"foo")(1,4,"foo")
-        (2,1,"bar")(2,2,"bar")(2,3,"bar")(2,4,"bar")
-        (3,1,"git")(3,2,"git")(3,3,"git");
-    test(input, validations);
-}
-
-void testLRorCR_bar_git(std::string const& line_break) {
-    std::string const input = line_break + "bar" + line_break + "git";
-    validations const validations = boost::assign::list_of<validation>
-        (1,1,"")
-        (2,1,"bar")(2,2,"bar")(2,3,"bar")(2,4,"bar")
-        (3,1,"git")(3,2,"git")(3,3,"git");
-    test(input, validations);
-}
-
-void testLRorCR_foo_bar(std::string const& line_break) {
-    std::string const input = "foo" + line_break + "bar" + line_break;
-    validations const validations = boost::assign::list_of<validation>
-        (1,1,"foo")(1,2,"foo")(1,3,"foo")(1,4,"foo")
-        (2,1,"bar")(2,2,"bar")(2,3,"bar")(2,4,"bar");
-    test(input, validations);
+void test(std::string const& line_break)
+{
+    test_only(line_break);
+    test_in_text(line_break);
+    test_at_begin(line_break);
+    test_at_end(line_break);
 }
 
 int main()
 {
-    testLRandCR("\r\n");
-    testLRandCR_foo_bar_git("\r\n");
-    testLRandCR_bar_git("\r\n");
-    testLRandCR_foo_bar("\r\n");
-
-    testLRandCR("\n\r");
-    testLRandCR_foo_bar_git("\n\r");
-    testLRandCR_bar_git("\n\r");
-    testLRandCR_foo_bar("\n\r");
-
-    testLRorCR("\r");
-    testLRorCR_foo_bar_git("\r");
-    testLRorCR_bar_git("\r");
-    testLRorCR_foo_bar("\r");
-
-    testLRorCR("\n");
-    testLRorCR_foo_bar_git("\n");
-    testLRorCR_bar_git("\n");
-    testLRorCR_foo_bar("\n");
+    test("\n");
+    test("\r\n");
+    test("\r");
+    test("\n\r");
 
     return boost::report_errors();
 }
