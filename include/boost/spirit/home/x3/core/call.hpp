3df9ca3788dfec5f54730284b5d2cbb2d40472a3
Nana Sakisaka <1901813+saki7@users.noreply.github.com>
2025-09-10 07:55:14 +0900

    Modernize `x3::action` (#815)
    `x3::action` now uses concepts to dispatch `f(ctx)` or `f()`, depending on 
    whether `f` accepts such signature.
    
    The dispatching implementation is moved from `call.hpp` to `action.hpp`,
     as the function is only used from the `x3::action` class.
    
    `operator/`: Deprecated. The symbol `/` normally means "ordered choice"
    in PEG, and is irrelevant to semantic actions. Furthermore, using C++'s 
    `operator/` for this purpose may introduce surprising behavior when it's 
    mixed with ordinary PEG operators, for instance, the unary `operator+`, 
    due to precedence.

--- include/boost/spirit/home/x3/core/call.hpp
+++ include/boost/spirit/home/x3/core/call.hpp
@@ -1,5 +1,6 @@
 /*=============================================================================
     Copyright (c) 2001-2014 Joel de Guzman
+    Copyright (c) 2025 Nana Sakisaka
 
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -7,70 +8,38 @@
 #if !defined(BOOST_SPIRIT_X3_CALL_CONTEXT_MAY_26_2014_0234PM)
 #define BOOST_SPIRIT_X3_CALL_CONTEXT_MAY_26_2014_0234PM
 
-#include <type_traits>
-
 #include <boost/spirit/home/x3/support/context.hpp>
-#include <boost/spirit/home/x3/support/utility/is_callable.hpp>
-#include <boost/range/iterator_range_core.hpp>
 
-namespace boost { namespace spirit { namespace x3
+namespace boost::spirit::x3
 {
-    ////////////////////////////////////////////////////////////////////////////
-    struct rule_val_context_tag;
+    struct parse_pass_context_tag; // _pass
+    struct rule_val_context_tag; // _val
+    struct where_context_tag; // _where
+    struct attr_context_tag; // _attr
 
     template <typename Context>
-    inline decltype(auto) _val(Context const& context)
+    [[nodiscard]] constexpr bool& _pass(Context const& context) noexcept
+    {
+        return x3::get<parse_pass_context_tag>(context);
+    }
+
+    template <typename Context>
+    [[nodiscard]] constexpr auto&& _val(Context const& context) noexcept
     {
         return x3::get<rule_val_context_tag>(context);
     }
 
-    ////////////////////////////////////////////////////////////////////////////
-    struct where_context_tag;
-
     template <typename Context>
-    inline decltype(auto) _where(Context const& context)
+    [[nodiscard]] constexpr auto&& _where(Context const& context) noexcept
     {
         return x3::get<where_context_tag>(context);
     }
 
-    ////////////////////////////////////////////////////////////////////////////
-    struct attr_context_tag;
-
     template <typename Context>
-    inline decltype(auto) _attr(Context const& context)
+    [[nodiscard]] constexpr auto&& _attr(Context const& context) noexcept
     {
         return x3::get<attr_context_tag>(context);
     }
-
-    ////////////////////////////////////////////////////////////////////////////
-    namespace detail
-    {
-        template <typename F, typename Context>
-        auto call(F f, Context const& context, mpl::true_)
-        {
-            return f(context);
-        }
-
-        template <typename F, typename Context>
-        auto call(F f, Context const& /* context */, mpl::false_)
-        {
-            return f();
-        }
-    }
-
-    template <
-        typename F, typename Iterator
-      , typename Context, typename RuleContext, typename Attribute>
-    auto call(
-        F f, Iterator& first, Iterator const& last
-      , Context const& context, RuleContext& rcontext, Attribute& attr)
-    {
-        boost::iterator_range<Iterator> rng(first, last);
-        auto val_context = make_context<rule_val_context_tag>(rcontext, context);
-        auto where_context = make_context<where_context_tag>(rng, val_context);
-        auto attr_context = make_context<attr_context_tag>(attr, where_context);
-        return detail::call(f, attr_context, is_callable<F(decltype(attr_context) const&)>());
-    }
-}}}
+} // boost::spirit::x3
 
 #endif
