e3c1389e9ade354e8d3a56f415243b86be91baf9
Nana Sakisaka <1901813+saki7@users.noreply.github.com>
2025-09-10 09:25:23 +0900

    Modernize `x3::matches`

--- test/x3/matches.cpp
+++ test/x3/matches.cpp
@@ -1,16 +1,18 @@
 /*=============================================================================
     Copyright (c) 2001-2015 Joel de Guzman
     Copyright (c) 2001-2010 Hartmut Kaiser
+    Copyright (c) 2025 Nana Sakisaka
 
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 =============================================================================*/
-#include <boost/spirit/home/x3.hpp>
-#include <iostream>
 #include "test.hpp"
 
-int
-main()
+#include <boost/spirit/home/x3.hpp>
+
+#include <iostream>
+
+int main()
 {
     using spirit_test::test;
     using spirit_test::test_attr;
