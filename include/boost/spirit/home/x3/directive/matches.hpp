26d6ec1a648aebeac4a393afdf768cb4902276f9
Nana Sakisaka <1901813+saki7@users.noreply.github.com>
2025-09-13 08:57:02 +0900

    Revert "Merge pull request #822 from saki7/modernize-matches"
    This reverts commit 495f936af243f7150f315ab8b96c6223db5caf6a, reversing
    changes made to b86852dd88a124ebfd0bfe399e1837d8d101af90.

--- include/boost/spirit/home/x3/directive/matches.hpp
+++ include/boost/spirit/home/x3/directive/matches.hpp
@@ -2,7 +2,7 @@
     Copyright (c) 2015 Mario Lang
     Copyright (c) 2001-2011 Hartmut Kaiser
     Copyright (c) 2017 wanghan02
-    Copyright (c) 2024-2025 Nana Sakisaka
+    Copyright (c) 2024 Nana Sakisaka
 
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -15,64 +15,45 @@
 #include <boost/spirit/home/x3/support/expectation.hpp>
 #include <boost/spirit/home/x3/support/unused.hpp>
 
-#include <iterator>
-#include <type_traits>
-#include <utility>
-
-namespace boost::spirit::x3
+namespace boost { namespace spirit { namespace x3
 {
     template <typename Subject>
     struct matches_directive : unary_parser<Subject, matches_directive<Subject>>
     {
         using base_type = unary_parser<Subject, matches_directive<Subject>>;
-        static constexpr bool has_attribute = true;
+        static bool const has_attribute = true;
         using attribute_type = bool;
 
-        template <typename SubjectT>
-            requires std::is_constructible_v<Subject, SubjectT>
-        constexpr matches_directive(SubjectT&& subject)
-            noexcept(std::is_nothrow_constructible_v<Subject, SubjectT>)
-            : base_type(std::forward<SubjectT>(subject))
-        {}
+        constexpr matches_directive(Subject const& subject) : base_type(subject) {}
 
-        template <std::forward_iterator It, std::sentinel_for<It> Se, typename Context, typename RContext, typename Attribute>
-        [[nodiscard]] constexpr bool
-        parse(It& first, Se const& last, Context const& context, RContext& rcontext, Attribute& attr) const
-            noexcept(
-                is_nothrow_parsable_v<Subject, It, Se, Context, RContext, unused_type> &&
-                noexcept(traits::move_to(std::declval<bool const&>(), attr))
-            )
+        template <typename Iterator, typename Context
+          , typename RContext, typename Attribute>
+        bool parse(Iterator& first, Iterator const& last
+          , Context const& context, RContext& rcontext, Attribute& attr) const
         {
-            bool const matched = this->subject.parse(first, last, context, rcontext, unused);
+            bool const result = this->subject.parse(
+                    first, last, context, rcontext, unused);
 
         #if !BOOST_SPIRIT_X3_THROW_EXPECTATION_FAILURE
-            if (x3::has_expectation_failure(context)) return false;
+            if (has_expectation_failure(context)) return false;
         #endif
 
-            traits::move_to(matched, attr);
+            traits::move_to(result, attr);
             return true;
         }
     };
 
-    namespace detail
+    struct matches_gen
     {
-        struct matches_gen
+        template <typename Subject>
+        constexpr matches_directive<typename extension::as_parser<Subject>::value_type>
+        operator[](Subject const& subject) const
         {
-            template <X3Subject Subject>
-            [[nodiscard]] constexpr matches_directive<as_parser_plain_t<Subject>>
-            operator[](Subject&& subject) const
-                noexcept(is_parser_nothrow_constructible_v<matches_directive<as_parser_plain_t<Subject>>, Subject>)
-            {
-                return { as_parser(std::forward<Subject>(subject)) };
-            }
-        };
-    } // detail
+            return { as_parser(subject) };
+        }
+    };
 
-    inline namespace cpos
-    {
-        inline constexpr detail::matches_gen matches{};
-    } // cpos
-
-} // boost::spirit::x3
+    constexpr auto matches = matches_gen{};
+}}}
 
 #endif
